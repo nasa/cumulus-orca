b3a471b1eba9042a400733fd5e5ac48dfabc388b
Baatz Lisa <lisa.baatz.ctr@usgs.gov>
2019-07-09 16:18:49 -0500

    updates for venv creation

--- bin/build_tasks.sh
+++ bin/build_tasks.sh
@@ -1,17 +1,16 @@
 #!/bin/sh
 
 set -e
-
+rm -rf venv
+python3 -m venv venv
 for TASK in `ls -d tasks/*/`
 do
   if [ ${TASK} != 'tasks/testfiles/' ]; then
     echo "Building `pwd`/${TASK}"
     cd "`pwd`/${TASK}"
     rm -rf build
-    rm -rf venv
     mkdir build
-    python3 -m venv venv
-    source venv/bin/activate
+    source ../../venv/bin/activate
     pip3 install -t build -r requirements.txt
     deactivate
     cp *.py build/
