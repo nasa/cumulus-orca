102e3de6498a980af27e151bd286fae17163a66d
AndrewDorn <dornadigital@gmail.com>
2020-11-20 15:35:49 -0600

    Update handler.py
    FIxed misunderstandings in my documentation.

--- tasks/copy_to_glacier/handler.py
+++ tasks/copy_to_glacier/handler.py
@@ -1,10 +1,18 @@
 from re import Match
-from typing import Dict, Any, List, Optional, AnyStr
+from typing import Dict, Any, List, Optional, AnyStr, Union
 
 from run_cumulus_task import run_cumulus_task
 import boto3
 import re
 import os
+
+CONFIG_FILE_STAGING_DIRECTORY_KEY = 'fileStagingDir'
+CONFIG_BUCKETS_KEY = 'buckets'
+CONFIG_COLLECTION_KEY = 'collection'
+
+COLLECTION_NAME_KEY = 'name'
+COLLECTION_VERSION_KEY = 'version'
+COLLECTION_URL_PATH_KEY = 'url_path'
 
 file_types_to_exclude = [".example"]  # ex: [".tar", ".gz"]
 
@@ -54,81 +62,61 @@ def get_source_bucket_and_key(granule_url) -> Optional[Match[AnyStr]]:
     Parses source bucket and key from s3 url.
     Args:
         granule_url: s3 url path to granule.
-    Returns:
-        re.Match object with argument [1] equal to source bucket and [2] equal to source key  todo: ?
+    Returns:  TODO: Strip 0th element from return value.
+        re.Match object with argument [1] equal to source bucket and [2] equal to source key.
     """
     return re.search("s3://([^/]*)/(.*)", granule_url)
 
 
-def get_bucket(filename: str, files: List[Dict[str, Any]]) -> str:
+def get_bucket(filename: str, collection_files: List[Dict[str, Any]]) -> str:
     """
-    Retrieves the first file {files} where the file's ['regex'] matches '*.'
-    And returns that file's ['bucket'] todo: ? Why this regex? Why only the first file?
+    Retrieves the first file pattern in {collection_files} where the file's ['regex'] matches the {filename}
+    And returns that file's ['bucket']
     Args:
         filename: Granule file name.
-        files: List of collection files.
+        collection_files: List of collection files.
             Each file is a dict with the following keys:
-                regex (todo): todo
-                bucket (todo): todo
+                regex (str): The regex that all files in the bucket must match with their name.
+                bucket (str): The name of the bucket containing the files.
     Returns:
         Bucket name.
     """
-    for file in files:
+    for file in collection_files:
         if re.match(file.get('regex', '*.'), filename):
-            return file['bucket']  # todo: This type of "take first" implies that user input should be better filtered.
+            return file['bucket']
     return 'public'
 
 
 # noinspection PyUnusedLocal
-def task(event: Dict[str, Any], context: object) -> Dict[str, Any]:
-    """todo
-
-    todo
+def task(event: Dict[str, Union[List[str], Dict]], context: object) -> Dict[str, Any]:
+    """
+    Copies the files in {event}['input'] from the collection specified in {config} to the {config}'s 'glacier' bucket.
 
     Args:
-        event: Event passed into the step from the aws workflow. A dict with the following keys:
-            input (list): A list of urls for granules to copy. Defaults to an empty list.
-            config (dict): A dict with the following keys:
-                collection (dict): todo: What is the context of this value?
-                    A dict with the following keys:
-                    name (str): todo: What is the context of this value?
-                        Used when generating the default value for {config}[fileStagingDir]
-                    version (str): todo: What is the context of this value?
-                        Used when constructing the default fileStagingDir.
-                    files (list[Dict]): A list of dicts representing files.
-                        The first file where the file's ['regex'] matches '*.'
-                        Is used to identify the bucket referenced in return's['granules'][filename]['files']['bucket']
-                        todo: The above doesn't seem intentional.
-                        Each dict contains the following keys:
-                            regex (str): todo
-                    url_path (str): Used when calling {copy_granule_between_buckets} as a part of the destination_key.
-
-                fileStagingDir (todo): todo. Presently unused except in output.
-                    Will default to name__version where 'name' and 'version' come from 'config[collection]'.
-                buckets (dict): A dict with the following keys:
-                    glacier (dict): A dict with the following keys:
-                        name (str): The name of the bucket to copy to.
-                url_path (str): todo: What is the context of this value?
-                    todo: Why do we have two url_paths, one which is used and one which is passed through?
-                    Is placed as the value of the return's['granules'][filename]['files']['url_path']
-                    Will default to the fileStagingDir.
+        event: Passed through from {handler}
 
 
         context: An object required by AWS Lambda. Unused.
 
     Returns:
         A dict with the following keys:
-            granules (List[Dict[str, Union[str, bytes, list]]]): TODO
-            input (list): The 'input' from the {event}.
+            granules (List[Dict[str, Union[str, bytes, list]]]): A list of dicts where each dict has the following keys:
+                path (str): config['fileStagingDir']
+                url_path (str): config['url_path'] if present, otherwise config['fileStagingDir']
+                bucket (str): The name of the config['buckets'] that matches the filename.
+                filename (str): The granule_url from event['input']
+                name (str): The granule_url from event['input']
+            input (list): event['input']
     """
     print(event)
     granule_urls = event.get('input', [])
     config = event.get('config')
-    collection = config.get('collection')
-    config['fileStagingDir'] = config.get('fileStagingDir',
-                                          f"{collection['name']}__{collection['version']}")
-    glacier_bucket = config.get('buckets').get('glacier').get('name')
-    url_path = collection.get('url_path')  # todo: This is not a safe variable name since url_path has multiple contexts
+    collection = config.get(CONFIG_COLLECTION_KEY)
+    config[CONFIG_FILE_STAGING_DIRECTORY_KEY] = \
+        config.get(CONFIG_FILE_STAGING_DIRECTORY_KEY,
+                   f"{collection[COLLECTION_NAME_KEY]}__{collection[COLLECTION_VERSION_KEY]}")
+    glacier_bucket = config.get(CONFIG_BUCKETS_KEY).get('glacier').get('name')
+    collection_url_path = collection.get(COLLECTION_URL_PATH_KEY)
     granule_data = {}
     for granule_url in granule_urls:
         filename = os.path.basename(granule_url)
@@ -136,12 +124,11 @@ def task(event: Dict[str, Any], context: object) -> Dict[str, Any]:
             granule_data[filename] = {'granuleId': filename, 'files': []}
         granule_data[filename]['files'].append(
             {
-                "path": config['fileStagingDir'],
-                "url_path": config.get('url_path', config['fileStagingDir']),
-                "bucket": get_bucket(filename, collection.get('files', [])),
-                # todo: Why is this get_bucket being called multiple times? 'files' will not change.
-                "filename": granule_url,
-                "name": granule_url
+                'path': config[CONFIG_FILE_STAGING_DIRECTORY_KEY],
+                'url_path': config.get('url_path', config[CONFIG_FILE_STAGING_DIRECTORY_KEY]),
+                'bucket': get_bucket(filename, collection.get('files', [])),
+                'filename': granule_url,
+                'name': granule_url
             }
         )
         if should_exclude_files_type(granule_url):
@@ -150,15 +137,49 @@ def task(event: Dict[str, Any], context: object) -> Dict[str, Any]:
         copy_granule_between_buckets(source_bucket=source[1],
                                      source_key=source[2],
                                      destination_bucket=glacier_bucket,
-                                     destination_key=f"{url_path}/{filename}")  # todo: url_path may not be present.
+                                     destination_key=f"{collection_url_path}/{filename}")
 
     final_output = list(granule_data.values())
     return {"granules": final_output, "input": granule_urls}
 
 
 # handler that is provided to aws lambda
-def handler(event, context):
-    """Lambda handler. todo copy docs from task once complete
+def handler(event: Dict[str, Union[List[str], Dict]], context: object) -> Any:
+    """Lambda handler. Runs a cumulus task that
+    copies the files in {event}['input'] from the collection specified in {config} to the {config}'s 'glacier' bucket.
+
+    Args:
+        event: Event passed into the step from the aws workflow. A dict with the following keys:
+            input (list): A list of urls for granules to copy. Defaults to an empty list.
+            config (dict): A dict with the following keys:
+                collection (dict): The collection from AWS.
+                    See https://nasa.github.io/cumulus/docs/data-cookbooks/sips-workflow
+                    A dict with the following keys:
+                    name (str): The name of the collection.
+                        Used when generating the default value for {config}[fileStagingDir]
+                    version (str): The version of the collection.
+                        Used when constructing the default fileStagingDir.
+                    files (list[Dict]): A list of dicts representing file types within the collection.
+                        The first file where the file's ['regex'] matches the filename from the input
+                        Is used to identify the bucket referenced in return's['granules'][filename]['files']['bucket']
+                        Each dict contains the following keys:
+                            regex (str): The regex that all files in the bucket must match with their name.
+                            bucket (str): The name of the bucket containing the files.
+                    url_path (str): Used when calling {copy_granule_between_buckets} as a part of the destination_key.
+
+                fileStagingDir (str): Unused except in output if url_path is missing.
+                    Will default to name__version where 'name' and 'version' come from 'config[collection]'.
+                buckets (dict): A dict with the following keys:
+                    glacier (dict): A dict with the following keys:
+                        name (str): The name of the bucket to copy to.
+                url_path (str): Is placed as the value of the return's['granules'][filename]['files']['url_path']
+                    Will default to the fileStagingDir.
+
+
+        context: An object required by AWS Lambda. Unused.
+
+    Returns:
+        The result of the cumulus task.
     """
     return run_cumulus_task(task, event, context)
 
