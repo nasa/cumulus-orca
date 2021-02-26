4e36429569a7f4466d3d8148189d80b607cecb28
Bradley Hazuka <bhazuka@contractor.usgs.gov>
2021-02-26 16:43:45 -0600

    [ORCA-100] Added terraform and aws provider sections. Split resources into security group and rule. Standardized naming convention to '_' instead of '-'. Added descriptions and updated variable names.

--- modules/security_groups/main.tf
+++ modules/security_groups/main.tf
@@ -1,20 +1,43 @@
-resource "aws_security_group" "vpc-postgres-ingress-all-egress" {
-  name   = "${var.prefix}-vpc-ingress-all-egress"
-  vpc_id = var.vpc_id
-
-  ingress {
-    from_port = var.egress_from_port
-    to_port   = var.egress_to_port
-    protocol  = "TCP"
-    self      = true
+## Terraform Requirements
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.5.0"
+    }
   }
+}
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 
-  tags = var.tags
+## AWS Provider Settings
+provider "aws" {
+  region  = var.region
+  profile = var.aws_profile
+}
+
+
+## Local Variables
+locals {
+  tags = merge(var.tags, { Deployment = var.prefix })
+}
+
+
+## Resources
+resource "aws_security_group" "vpc_postgres_ingress_all_egress" {
+  description            = "ORCA security group to allow PostgreSQL access."
+  name                   = "${var.prefix}-vpc-postgres-ingress-all-egress"
+  revoke_rules_on_delete = true
+  tags                   = local.tags
+  vpc_id                 = var.vpc_id
+}
+
+
+resource "aws_security_group_rule" "rds_security_group_allow_postgres" {
+  description       = "ORCA security group rule to allow PostgreSQL port traffic."
+  from_port         = var.database_port
+  protocol          = "tcp"
+  security_group_id = aws_security_group.vpc_postgres_ingress_all_egress.id
+  self              = true
+  to_port           = var.database_port
+  type              = "ingress"
 }
