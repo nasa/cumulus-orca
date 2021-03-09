8ef4a689981dcb01e97bd10e6f7036ac15920e7e
Bradley Hazuka <bhazuka@contractor.usgs.gov>
2021-03-09 11:35:19 -0600

    [ORCA-100] Added additional documentation to resource and indicated required and optional items.

--- modules/security_groups/main.tf
+++ modules/security_groups/main.tf
@@ -22,8 +22,12 @@ locals {
 }
 
 
-## Resources
+## Security Group Resources
+
+## vpc_postgres_ingress_all_egress - PostgreSQL security Group
+## ==============================================================================
 resource "aws_security_group" "vpc_postgres_ingress_all_egress" {
+  ## OPTIONAL
   description            = "ORCA security group to allow PostgreSQL access."
   name                   = "${var.prefix}-vpc-postgres-ingress-all-egress"
   revoke_rules_on_delete = true
@@ -31,13 +35,17 @@ resource "aws_security_group" "vpc_postgres_ingress_all_egress" {
   vpc_id                 = var.vpc_id
 }
 
-
+# rds_security_group_allow_postgres - PostgreSQL ingress port rules
+# ------------------------------------------------------------------------------
 resource "aws_security_group_rule" "rds_security_group_allow_postgres" {
-  description       = "ORCA security group rule to allow PostgreSQL port traffic."
+  ## REQUIRED
   from_port         = var.database_port
   protocol          = "tcp"
   security_group_id = aws_security_group.vpc_postgres_ingress_all_egress.id
-  self              = true
   to_port           = var.database_port
   type              = "ingress"
+
+  ## OPTIONAL
+  description = "ORCA security group rule to allow PostgreSQL port traffic."
+  self        = true
 }
