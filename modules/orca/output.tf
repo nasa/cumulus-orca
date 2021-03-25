6bd32f9fa235666014540532a9ca38cc5250ea4a
Bradley Hazuka <bhazuka@contractor.usgs.gov>
2021-03-25 11:42:11 -0500

    Fixed validation issues with the output.

--- modules/orca/output.tf
+++ modules/orca/output.tf
@@ -58,27 +58,34 @@ output "orca_rds" {
   value       = module.orca_rds.rds
 }
 
+
 output "orca_subnet_group" {
   description = "PostgreSQL subnet group object. Used only for development validation."
   value       = module.orca_rds.rds_subnet_group
 }
 
+
+## SQS Module Outputs (orca_sqs)
+## =============================================================================
 output "orca_sqs_staged_recovery_queue_arn" {
   description = "The ARN of the staged-recovery-queue SQS"
-  value       = module.orca_sqs.staged_recovery_queue_arn
+  value       = module.orca_sqs.orca_sqs_staged_recovery_queue_arn
 }
+
 
 output "orca_sqs_staged_recovery_queue_id" {
   description = "The URL ID of the staged-recovery-queue SQS"
-  value       = module.orca_sqs.staged_recovery_queue_id
+  value       = module.orca_sqs.orca_sqs_staged_recovery_queue_id
 }
+
 
 output "orca_sqs_status_update_queue_arn" {
   description = "The ARN of the status-update-queue SQS"
-  value       = module.orca_sqs.status_update_queue_arn
+  value       = module.orca_sqs.orca_sqs_status_update_queue_arn
 }
+
 
 output "orca_sqs_status_update_queue_id" {
   description = "The URL ID of the status-update-queue SQS"
-  value       = module.orca_sqs.status_update_queue_id
+  value       = module.orca_sqs.orca_sqs_status_update_queue_id
 }
