55086c338964428ab3864072e212beaa50309bd1
Dorn <adorn@contractor.usgs.gov>
2021-04-01 14:46:39 -0500

    post_to_database
    Added tests and docs to post_to_database.

--- tasks/post_to_database/post_to_database.py
+++ tasks/post_to_database/post_to_database.py
@@ -6,14 +6,12 @@ to another s3 bucket.
 """
 import json
 from enum import Enum
-from typing import Any, List, Dict, Optional
+from typing import Any, List, Dict
 
-from botocore.client import BaseClient
-from botocore.exceptions import ClientError
-# noinspection PyUnresolvedReferences
-import requests_db
 # noinspection PyPackageRequirements
 import database
+# noinspection PyUnresolvedReferences
+import requests_db
 from cumulus_logger import CumulusLogger
 
 
@@ -27,24 +25,49 @@ LOGGER = CumulusLogger()
 
 def task(records: List[Dict[str, Any]], db_connect_info: Dict):
     for record in records:
-        post_record_to_database(record, db_connect_info)
+        send_record_to_database(record, db_connect_info)
 
 
-def post_record_to_database(record: Dict[str, Any], db_connect_info: Dict):
+def send_record_to_database(record: Dict[str, Any], db_connect_info: Dict) -> None:
+    """
+    Deconstructs a record to its components and calls send_values_to_database with the result.
+
+    Args:
+        record: Contains the following keys:
+            'body' (str): A json string representing a dict.
+                Contains key/value pairs of column names and values for those columns.
+            'messageAttributes' (dict): Contains the following keys:
+                'TableName' (str): The name of the table to target.
+                'RequestMethod' (str): 'post' or 'put', depending on if row should be created or updated respectively.
+        db_connect_info: See requests_db.py's get_dbconnect_info for further details.
+    """
     values = json.loads(record['body'])
     table_name = record['messageAttributes']['TableName']
-    override_existing = RequestMethod(record['messageAttributes']['RequestMethod'])
-    post_values_to_database(table_name, values, override_existing, db_connect_info)
+    request_method = RequestMethod(record['messageAttributes']['RequestMethod'])
+    send_values_to_database(table_name, values, request_method, db_connect_info)
 
 
-def post_values_to_database(table_name: str, values: Dict[str, Any], request_method: RequestMethod,
-                            db_connect_info: Dict):
+table_key_dictionary = {
+    'orca_recoverfile': ['job_id', 'granule_id', 'filename'],
+    'orca_recoveryjob': ['job_id', 'granule_id']
+}
+
+
+def send_values_to_database(table_name: str, values: Dict[str, Any], request_method: RequestMethod,
+                            db_connect_info: Dict) -> None:
+    """
+
+    Args:
+        table_name: The name of the table to target.
+        values: Contains key/value pairs of column names and values for those columns.
+        request_method: POST or PUT, depending on if row should be created or updated respectively.
+        db_connect_info: See requests_db.py's get_dbconnect_info for further details.
+    """
     try:
         if request_method == RequestMethod.PUT:
-            if table_name == 'orca_recoverfile':
-                update_orca_recoverfile(values, db_connect_info)
-            elif table_name == 'orca_recoveryjob':
-                update_orca_recoveryjob(values, db_connect_info)
+            keys = table_key_dictionary.get(table_name, None)
+            if keys is not None:
+                update_row_in_table(table_name, keys, values, db_connect_info)
             else:
                 raise NotImplementedError()
         elif request_method == RequestMethod.POST:
@@ -53,7 +76,17 @@ def post_values_to_database(table_name: str, values: Dict[str, Any], request_met
         LOGGER.error(ex)
 
 
-def insert_row_from_values(table_name: str, values: Dict[str, Any], db_connect_info: Dict):
+def insert_row_from_values(table_name: str, values: Dict[str, Any], db_connect_info: Dict) -> None:
+    """
+    Inserts a new row into the given table.
+
+    Args:
+        table_name: The name of the table to target.
+        values: Contains key/value pairs of column names and values for those columns.
+        db_connect_info: See requests_db.py's get_dbconnect_info for further details.
+
+    Raises: database.DbError if error occurs when contacting database.
+    """
     sql = f"""
                     INSERT INTO {table_name} ("""
     sql += ', '.join(values.keys())
@@ -66,15 +99,19 @@ def insert_row_from_values(table_name: str, values: Dict[str, Any], db_connect_i
     database.single_query(sql, db_connect_info, parameters)
 
 
-def update_orca_recoverfile(values: Dict[str, Any], db_connect_info: Dict):
-    update_row_in_table('orca_recoverfile', ['job_id', 'granule_id', 'filename'], values, db_connect_info)
+def update_row_in_table(table_name: str, table_keys: List[str], values: Dict[str, Any], db_connect_info: Dict) -> None:
+    """
+    Updates a row in the target table, using table_keys to identify the row that should be modified.
 
+    Args:
+        table_name: The name of the table to target.
+        table_keys: A list of keys. Used to identify the row to change.
+        values: Contains key/value pairs of column names and values for those columns.
+            If a column name is in table_keys, the value will be used to identify the target row.
+        db_connect_info: See requests_db.py's get_dbconnect_info for further details.
 
-def update_orca_recoveryjob(values: Dict[str, Any], db_connect_info: Dict):
-    update_row_in_table('orca_recoveryjob', ['job_id', 'granule_id'], values, db_connect_info)
-
-
-def update_row_in_table(table_name: str, table_keys: List[str], values: Dict[str, Any], db_connect_info: Dict):
+    Raises: database.DbError if error occurs when contacting database.
+    """
     key_values = {table_key: None for table_key in table_keys}
     updated_values = {}
     for key in values:
@@ -93,20 +130,16 @@ def update_row_in_table(table_name: str, table_keys: List[str], values: Dict[str
 
     sql += " AND ".join([f"{table_key} = %s" for table_key in table_keys])
 
-#    sql += f"""
-#                    LIMIT 1"""
+    #    sql += f"""
+    #                    LIMIT 1"""
 
     parameters = tuple(updated_values[updated_value_key] for updated_value_key in updated_values)
     parameters += tuple(key_values[table_key] for table_key in table_keys)
-    try:
-        database.single_query(sql, db_connect_info, parameters)
-    except database.DbError as err:
-        LOGGER.error(f"DbError: {str(err)}")
-        raise requests_db.DatabaseError(str(err))
+    database.single_query(sql, db_connect_info, parameters)
 
 
 # todo: Make sure to grant sqs:ReceiveMessage, sqs:DeleteMessage, and sqs:GetQueueAttributes
-def handler(event: Dict[str, List], context):
+def handler(event: Dict[str, List], context) -> None:
     """
     Lambda handler. Receives a list of queue entries from an SQS queue, and posts them to a database.
 
@@ -115,13 +148,12 @@ def handler(event: Dict[str, List], context):
             'Records' (List): A list of dicts with the following keys:
                 'messageId' (str)
                 'receiptHandle' (str)
-                'body' (str): A json formatted string representing a dict of values to write as DB entry.
+                'body' (str): A json string representing a dict.
+                    Contains key/value pairs of column names and values for those columns.
                 'attributes' (Dict)
                 'messageAttributes' (Dict): A dict with the following keys defined in the functions that write to queue.
-                    'RequestMethod' (str): 'post' or 'put'.
-                        If post, no entry with the same keys can exist.
-                        If put, an entry with the same keys must exist, and will be updated with the values in 'body'.
-                    'TableName': (str): The name of the table to write to.
+                    'RequestMethod' (str): 'post' or 'put', depending on if row should be created or updated respectively.
+                    'TableName' (str): The name of the table to target.
         context: An object passed through by AWS. Used for tracking.
     Environment Vars: See requests_db.py's get_dbconnect_info for further details.
         'DATABASE_PORT' (int): Defaults to 5432
@@ -137,4 +169,3 @@ def handler(event: Dict[str, List], context):
 
     task(event['Records'], db_connect_info)
     pass
-
---
+++ tasks/post_to_database/test/unit_tests/test_post_to_database.py
@@ -0,0 +1,203 @@
+"""
+Name: test_post_to_database.py
+
+Description:  Unit tests for test_post_to_database.py.
+"""
+import json
+import unittest
+import uuid
+from unittest.mock import Mock, call, patch, MagicMock
+
+import post_to_database
+
+
+class TestPostToDatabase(unittest.TestCase):  # pylint: disable-msg=too-many-instance-attributes
+    """
+    TestPostToDatabase.
+    """
+
+    @patch('post_to_database.task')
+    @patch('requests_db.get_dbconnect_info')
+    @patch('cumulus_logger.CumulusLogger.setMetadata')
+    def test_handler_happy_path(self,
+                                mock_set_metadata: MagicMock,
+                                mock_get_dbconnect_info: MagicMock,
+                                mock_task: MagicMock):
+        records = Mock()
+        event = {'Records': records}
+        context = Mock()
+        post_to_database.handler(event, context)
+        mock_set_metadata.assert_called_once_with(event, context)
+        mock_task.assert_called_once_with(records, mock_get_dbconnect_info.return_value)
+
+    @patch('post_to_database.send_record_to_database')
+    def test_task_happy_path(self,
+                             mock_send_record_to_database: MagicMock):
+        record0 = Mock()
+        record1 = Mock()
+        records = [record0, record1]
+        db_connect_info = Mock()
+
+        post_to_database.task(records, db_connect_info)
+
+        mock_send_record_to_database.assert_has_calls([call(record0, db_connect_info), call(record1, db_connect_info)])
+        self.assertEqual(2, mock_send_record_to_database.call_count)
+
+    @patch('post_to_database.send_values_to_database')
+    def test_send_record_to_database(self,
+                                     mock_send_values_to_database: MagicMock):
+        table_name = uuid.uuid4().__str__()
+        values = {uuid.uuid4().__str__(): uuid.uuid4().__str__()}
+        request_method = post_to_database.RequestMethod.PUT
+        db_connect_info = Mock()
+        record = {
+            'body': json.dumps(values, indent=4),
+            'messageAttributes': {'TableName': table_name, 'RequestMethod': request_method.value.__str__()}
+        }
+
+        post_to_database.send_record_to_database(record, db_connect_info)
+
+        mock_send_values_to_database.assert_called_once_with(table_name, values, request_method, db_connect_info)
+
+    @patch('post_to_database.update_row_in_table')
+    def test_send_values_to_database_PUT_orca_recoverfile(self,
+                                                          mock_update_row_in_table: MagicMock):
+        """
+        Use correct keys when updating a table.
+        """
+        table_name = 'orca_recoverfile'
+        values = Mock()
+        db_connect_info = Mock()
+
+        post_to_database.send_values_to_database(table_name, values, post_to_database.RequestMethod.PUT,
+                                                 db_connect_info)
+
+        mock_update_row_in_table.assert_called_once_with(table_name, ['job_id', 'granule_id', 'filename'], values,
+                                                         db_connect_info)
+
+    @patch('post_to_database.update_row_in_table')
+    def test_send_values_to_database_PUT_orca_recoveryjob(self,
+                                                          mock_update_row_in_table: MagicMock):
+        """
+        Use correct keys when updating a table.
+        """
+        table_name = 'orca_recoveryjob'
+        values = Mock()
+        db_connect_info = Mock()
+
+        post_to_database.send_values_to_database(table_name, values, post_to_database.RequestMethod.PUT,
+                                                 db_connect_info)
+
+        mock_update_row_in_table.assert_called_once_with(table_name, ['job_id', 'granule_id'], values,
+                                                         db_connect_info)
+
+    @patch('cumulus_logger.CumulusLogger.error')
+    @patch('post_to_database.update_row_in_table')
+    def test_send_values_to_database_PUT_error_logs(self,
+                                                    mock_update_row_in_table: MagicMock,
+                                                    mock_error: MagicMock):
+        """
+        Use correct keys when updating a table.
+        """
+        table_name = 'orca_recoverfile'
+        values = Mock()
+        db_connect_info = Mock()
+        error = Exception()
+        mock_update_row_in_table.side_effect = error
+
+        post_to_database.send_values_to_database(table_name, values, post_to_database.RequestMethod.PUT,
+                                                 db_connect_info)
+
+        mock_update_row_in_table.assert_called_once_with(table_name, ['job_id', 'granule_id', 'filename'], values,
+                                                         db_connect_info)
+        mock_error.assert_called_once_with(error)
+
+    @patch('cumulus_logger.CumulusLogger.error')
+    @patch('post_to_database.update_row_in_table')
+    def test_send_values_to_database_bad_table_logs(self,
+                                                    mock_update_row_in_table: MagicMock,
+                                                    mock_error: MagicMock):
+        """
+        If table doesn't have known keys, can't post.
+        """
+        table_name = uuid.uuid4().__str__()
+        values = Mock()
+        db_connect_info = Mock()
+
+        post_to_database.send_values_to_database(table_name, values, post_to_database.RequestMethod.PUT,
+                                                 db_connect_info)
+        mock_update_row_in_table.assert_not_called()
+        mock_error.assert_called_once()
+
+    @patch('post_to_database.insert_row_from_values')
+    def test_send_values_to_database_post_happy_path(self,
+                                                     mock_insert_row_from_values: MagicMock):
+        table_name = uuid.uuid4().__str__()
+        values = Mock()
+        db_connect_info = Mock()
+
+        post_to_database.send_values_to_database(table_name, values, post_to_database.RequestMethod.POST,
+                                                 db_connect_info)
+
+        mock_insert_row_from_values.assert_called_once_with(table_name, values, db_connect_info)
+
+    @patch('cumulus_logger.CumulusLogger.error')
+    @patch('post_to_database.insert_row_from_values')
+    def test_send_values_to_database_POST_error_logs(self,
+                                                     mock_insert_row_from_values: MagicMock,
+                                                     mock_error: MagicMock):
+        """
+        Use correct keys when updating a table.
+        """
+        table_name = uuid.uuid4().__str__()
+        values = Mock()
+        db_connect_info = Mock()
+        error = Exception()
+        mock_insert_row_from_values.side_effect = error
+
+        post_to_database.send_values_to_database(table_name, values, post_to_database.RequestMethod.POST,
+                                                 db_connect_info)
+
+        mock_insert_row_from_values.assert_called_once_with(table_name, values, db_connect_info)
+        mock_error.assert_called_once_with(error)
+
+    @patch('database.single_query')
+    def test_insert_row_from_values_happy_path(self,
+                                               mock_single_query: MagicMock):
+        table_name = uuid.uuid4().__str__()
+        column0 = uuid.uuid4().__str__()
+        value0 = uuid.uuid4().__str__()
+        column1 = uuid.uuid4().__str__()
+        value1 = uuid.uuid4().__str__()
+        values = {column0: value0, column1: value1}
+        db_connect_info = Mock()
+
+        post_to_database.insert_row_from_values(table_name, values, db_connect_info)
+
+        mock_single_query.assert_called_once_with(
+            f"\n                    INSERT INTO {table_name} ({column0}, {column1})\n"
+            f"                    VALUES (%s, %s)",
+            db_connect_info, (value0, value1,))
+
+    @patch('database.single_query')
+    def test_update_row_in_table_happy_path(self,
+                                            mock_single_query: MagicMock):
+        table_name = uuid.uuid4().__str__()
+        key0 = uuid.uuid4().__str__()
+        key_value0 = uuid.uuid4().__str__()
+        key1 = uuid.uuid4().__str__()
+        key_value1 = uuid.uuid4().__str__()
+        column0 = uuid.uuid4().__str__()
+        value0 = uuid.uuid4().__str__()
+        column1 = uuid.uuid4().__str__()
+        value1 = uuid.uuid4().__str__()
+        values = {key0: key_value0, key1: key_value1, column0: value0, column1: value1}
+        db_connect_info = Mock()
+
+        post_to_database.update_row_in_table(table_name, [key0, key1], values, db_connect_info)
+
+        mock_single_query.assert_called_once_with(
+            f"\n                    UPDATE {table_name}\n"
+            f"                    SET {column0} = %s, {column1} = %s\n"
+            f"                    WHERE {key0} = %s AND {key1} = %s",
+            db_connect_info, (value0, value1, key_value0, key_value1))
