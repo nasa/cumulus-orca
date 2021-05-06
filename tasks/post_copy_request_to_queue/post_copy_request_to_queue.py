4d27c820901a904629ad37cbf91771c04150b7ee
Rizbi <rizbiece@gmail.com>
2021-05-06 13:08:15 +0000

    making changes according to review comments

--- tasks/post_copy_request_to_queue/post_copy_request_to_queue.py
+++ tasks/post_copy_request_to_queue/post_copy_request_to_queue.py
@@ -14,7 +14,7 @@ from shared_recovery import (
     OrcaStatus,
     RequestMethod,
     post_status_for_file_to_queue,
-    post_status_for_job_to_queue,
+    post_entry_to_queue,
 )
 from requests_db import get_dbconnect_info
 from database import single_query, result_to_json
@@ -24,7 +24,8 @@ from cumulus_logger import CumulusLogger
 # instantiate CumulusLogger
 LOGGER = CumulusLogger()
 
-def task(records, db_queue_url, recovery_queue_url):
+
+def task(records: Dict[str, Any], db_queue_url: str, recovery_queue_url: str) -> None:
     """
     Task called by the handler to perform the work.
 
@@ -36,11 +37,18 @@ def task(records, db_queue_url, recovery_queue_url):
         records: A dictionary passed through from the handler.
         db_queue_url: The SQS URL of status_update_queue
         recovery_queue_url: The SQS URL of staged_recovery_queue
+    Returns:
+        None
+    Raises:
+        Exception: If unable to retrieve filename or db parameters, convert db result to json,
+        or post to queue.
+
     """
 
-    # grab the filename from event
+    # grab the filename and bucketname from event
     for record in records:
         filename = record["s3"]["object"]["key"]
+        bucket_name = record["s3"]["bucket"]["name"]
 
     sql = """
         SELECT
@@ -59,49 +67,76 @@ def task(records, db_queue_url, recovery_queue_url):
     try:
         rows = single_query(sql, db_connect_info, (filename, OrcaStatus.PENDING.value))
     except Exception as ex:
-        LOGGER.error(ex)
+        LOGGER.error("Unable to retrieve {filename} metadata")
+        raise ex
 
     if len(rows) == 0:
         LOGGER.fatal("DB tables cannot be empty")
-        raise Exception(
-            "No metadata found for {filename}"
-        )
+        raise Exception("No metadata found for {filename}")
     # convert db result to json
     try:
         db_result_json = result_to_json(rows)
-    except Exception as e:
+    except Exception as ex:
+        LOGGER.error("Unable to convert db result to json")
+        raise ex
+
+    # grab the parameters from the db in json format. Retry 3 times if it fails
+    for retry in range(3):
+        try:
+            job_id = db_result_json["job_id"]
+            granule_id = db_result_json["granule_id"]
+            restore_destination = db_result_json["restore_destination"]
+        except Exception:
+            LOGGER.error(
+                "Unable to retrieve the parameters from db result. Retrying..."
+            )
+            continue
+        break
+    else:
+        raise Exception("Unable to retrieve the parameters from db result")
+
+    # post to recovery queue for copy_files_to_archive lambda. Retry 3 times if it fails
+    new_data = {
+        "job_id": job_id,
+        "granule_id": granule_id,
+        "filename": filename,
+        "restore_destination": restore_destination,
+        "source_bucket": bucket_name
+    }
+    for retry in range(3):
+        try:
+            post_entry_to_queue(
+                orca_recoveryfile, new_data, RequestMethod.NEW.value, recovery_queue_url
+            )
+        except Exception:
+            LOGGER.error("Unable to send message to recovery SQS. Retrying...")
+            continue
+        break
+    else:
+        raise Exception("Unable to send message to recovery SQS.")
+
+    # post to DB-queue. Retry 3 times if it fails
+    for retry in range(3):
+        try:
+            post_status_for_file_to_queue(
+                job_id,
+                granule_id,
+                filename,
+                restore_destination,
+                OrcaStatus.STAGED.value,
+                None,
+                RequestMethod.UPDATE.value,
+                db_queue_url,
+            )
+        except Exception:
+            LOGGER.error("Unable to send message to db SQS. Retrying...")
+            continue
+        break
+    else:
+        raise Exception("Unable to send message to db SQS.")
 
 
-    # grab the parameters from the db in json format
-    job_id = db_result_json["job_id"]
-    granule_id = db_result_json["granule_id"]
-    restore_destination = db_result_json["restore_destination"]
-
-    # post to staged-recovery-queue
-    post_status_for_file_to_queue(
-        job_id,
-        granule_id,
-        filename,
-        restore_destination,
-        OrcaStatus.STAGED.value,
-        None,
-        RequestMethod.NEW.value,
-        RECOVERY_QUEUE_URL,
-    )
-
-    # post to DB-queue
-    post_status_for_file_to_queue(
-        job_id,
-        granule_id,
-        filename,
-        restore_destination,
-        OrcaStatus.STAGED.value,
-        None,
-        RequestMethod.UPDATE.value,
-        DB_QUEUE_URL,
-    )
-
-def handler(event, context):
+def handler(event: Dict[str, Any], context: None) -> None:
     """
     Lambda handler. This lambda calls the task function to perform db queries
     and send message to SQS.
@@ -120,8 +155,10 @@ def handler(event, context):
         event:
             A dictionary from the S3 bucket. See schemas/input.json for more information.
         context: An object required by AWS Lambda. Unused.
-    Raises:
+    Returns:
         None
+    Raises:
+        Exception: If unable to retrieve the SQS URLs from env variables.
 
     """
     LOGGER.setMetadata(event, context)
