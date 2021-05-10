0064c35760ad829bc98a97a0056e55d3c5766640
Dorn <adorn@contractor.usgs.gov>
2021-05-10 12:21:40 -0500

    Update shared_recovery.py

--- tasks/post_to_database/orca_shared/shared_recovery.py
+++ tasks/post_to_database/orca_shared/shared_recovery.py
@@ -44,24 +44,31 @@ def create_status_for_job(
         job_id: The unique identifier used for tracking requests.
         granule_id: The id of the granule being restored.
         archive_destination: The S3 bucket destination of where the data is archived.
-        files: TODO
+        files: A List of Dicts with the following keys:
+            'filename' (str)
+            'key_path' (str)
+            'restore_destination' (str)
+            'status_id' (int)
+            'error_message' (str, Optional)
+            'request_time' (str)
+            'last_update' (str)
+            'completion_time' (str, Optional)
         db_queue_url: The SQS queue URL defined by AWS.
-
     """
     new_data = {"job_id": job_id, "granule_id": granule_id,
                 "request_time": datetime.now(timezone.utc).isoformat(), "archive_destination": archive_destination,
                 "files": files}
 
-    post_entry_to_queue("orca_recoveryjob", new_data, RequestMethod.NEW_JOB, db_queue_url)
+    post_entry_to_queue(new_data, RequestMethod.NEW_JOB, db_queue_url)
 
 
 def update_status_for_file(
     job_id: str,
     granule_id: str,
     filename: str,
-    status_id: OrcaStatus,
+    orca_status: OrcaStatus,
     error_message: Optional[str],
-    db_queue_url: str,
+    db_queue_url: str
 ):
     """
     Creates update information for a file's status entry, and posts to queue.
@@ -71,7 +78,7 @@ def update_status_for_file(
         job_id: The unique identifier used for tracking requests.
         granule_id: The id of the granule being restored.
         filename: The name of the file being copied.
-        status_id: Defines the status id used in the ORCA Recovery database.
+        orca_status: Defines the status id used in the ORCA Recovery database.
         error_message: message displayed on error.
         db_queue_url: The SQS queue URL defined by AWS.
     """
@@ -81,21 +88,20 @@ def update_status_for_file(
         "granule_id": granule_id,
         "filename": filename,
         "last_update": last_update,
-        "status_id": status_id.value,
+        "status_id": orca_status.value,
     }
 
-    if status_id == OrcaStatus.SUCCESS or status_id == OrcaStatus.FAILED:
+    if orca_status == OrcaStatus.SUCCESS or orca_status == OrcaStatus.FAILED:
         new_data["completion_time"] = datetime.now(timezone.utc).isoformat()
-        if status_id == OrcaStatus.FAILED:
+        if orca_status == OrcaStatus.FAILED:
             if len(error_message) == 0 or error_message is None:
                 raise Exception("error message is required.")
             new_data["error_message"] = error_message
 
-    post_entry_to_queue("orca_recoverfile", new_data, RequestMethod.UPDATE, db_queue_url)
+    post_entry_to_queue(new_data, RequestMethod.UPDATE_FILE, db_queue_url)
 
 
 def post_entry_to_queue(
-    table_name: str,
     new_data: Dict[str, Any],
     request_method: RequestMethod,
     db_queue_url: str,
@@ -104,7 +110,6 @@ def post_entry_to_queue(
     Posts messages to an SQS queue.
 
     Args:
-        table_name: The name of the DB table.
         new_data: A dictionary representing the column/value pairs to write to the DB table.
         request_method: The method action for the database lambda to take when posting to the SQS queue.
         db_queue_url: The SQS queue URL defined by AWS.
@@ -114,19 +119,21 @@ def post_entry_to_queue(
     """
     body = json.dumps(new_data)
 
-    mysqs_resource = boto3.resource("sqs")
+    # TODO: pass AWS region value to function. SHOULD be gotten from environment
+    # higher up. Setting this to us-west-2 initially since that is where
+    # EOSDIS runs from normally. SEE ORCA-203 https://bugs.earthdata.nasa.ov/browse/ORCA-203
+    mysqs_resource = boto3.resource("sqs", region_name="us-west-2")
     mysqs = mysqs_resource.Queue(db_queue_url)
 
     mysqs.send_message(
         QueueUrl=db_queue_url,
-        MessageDeduplicationId=table_name + request_method.value + body,
+        MessageDeduplicationId=request_method.value + body,
         MessageGroupId="request_files",
         MessageAttributes={
             "RequestMethod": {
                 "DataType": "String",
                 "StringValue": request_method.value,
-            },
-            "TableName": {"DataType": "String", "StringValue": table_name},
+            }
         },
         MessageBody=body,
     )
