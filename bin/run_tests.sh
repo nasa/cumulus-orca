0eab7a383aac326e96543e14476923978016ae1b
Rizbi <rizbiece@gmail.com>
2021-05-26 15:12:50 +0000

    trying to fix conflict

--- bin/run_tests.sh
+++ bin/run_tests.sh
@@ -5,7 +5,7 @@ base=$(pwd)
 failed=0
 
 # Crawl the task directories
-for taskdir in `ls -d tasks/* | egrep -v 'request_status_for|shared_libraries|copy_files_to_archive' `
+for taskdir in `ls -d tasks/* | egrep -v 'request_status_for|shared_libraries|db_deploy|package|copy_files_to_archive' `
 do
   # Build and run tests for each task directory
   cd $taskdir
@@ -47,7 +47,7 @@ done
 
 ## Call each tasks testing suite
 ## TODO: Add more logging output and possibly make asynchronus
-for task in $(ls -d tasks/* | egrep 'request_status_|copy_files_to_archive')
+for task in $(ls -d tasks/* | egrep "request_status_|db_deploy|copy_files_to_archive")
 do
   echo
   echo "Running tests in $task"
