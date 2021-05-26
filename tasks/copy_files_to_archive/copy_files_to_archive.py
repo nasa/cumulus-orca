ed7e81ce17a31250e236ba58304a69530c577339
Rizbi <rizbiece@gmail.com>
2021-05-26 14:14:35 +0000

    formatted .py files, updated build and test scripts

--- tasks/copy_files_to_archive/copy_files_to_archive.py
+++ tasks/copy_files_to_archive/copy_files_to_archive.py
@@ -13,33 +13,42 @@ from typing import Any, List, Dict, Optional, Union
 import fastjsonschema
 
 import boto3
+
 # noinspection PyPackageRequirements
 from botocore.client import BaseClient
+
 # noinspection PyPackageRequirements
 from botocore.exceptions import ClientError
 from orca_shared import shared_recovery
 
-OS_ENVIRON_DB_QUEUE_URL_KEY = 'DB_QUEUE_URL'
+OS_ENVIRON_DB_QUEUE_URL_KEY = "DB_QUEUE_URL"
 
 # These will determine what the output looks like.
-FILE_SUCCESS_KEY = 'success'
-FILE_ERROR_MESSAGE_KEY = 'err_msg'
+FILE_SUCCESS_KEY = "success"
+FILE_ERROR_MESSAGE_KEY = "err_msg"
 
 # These are tied to the input schema.
-INPUT_JOB_ID_KEY = 'job_id'
-INPUT_GRANULE_ID_KEY = 'granule_id'
-INPUT_FILENAME_KEY = 'filename'
-INPUT_SOURCE_KEY_KEY = 'source_key'
-INPUT_TARGET_KEY_KEY = 'target_key'
-INPUT_TARGET_BUCKET_KEY = 'restore_destination'
-INPUT_SOURCE_BUCKET_KEY = 'source_bucket'
+INPUT_JOB_ID_KEY = "job_id"
+INPUT_GRANULE_ID_KEY = "granule_id"
+INPUT_FILENAME_KEY = "filename"
+INPUT_SOURCE_KEY_KEY = "source_key"
+INPUT_TARGET_KEY_KEY = "target_key"
+INPUT_TARGET_BUCKET_KEY = "restore_destination"
+INPUT_SOURCE_BUCKET_KEY = "source_bucket"
+
 
 class CopyRequestError(Exception):
     """
     Exception to be raised if the copy request fails for any of the files.
     """
 
-def task(records: List[Dict[str, Any]], max_retries: int, retry_sleep_secs: float, db_queue_url: str) -> None:
+
+def task(
+    records: List[Dict[str, Any]],
+    max_retries: int,
+    retry_sleep_secs: float,
+    db_queue_url: str,
+) -> None:
     """
     Task called by the handler to perform the work.
     This task will call copy_object for each file. A copy will be tried
@@ -55,29 +64,36 @@ def task(records: List[Dict[str, Any]], max_retries: int, retry_sleep_secs: floa
         CopyRequestError: Thrown if there are errors with the input records or the copy failed.
     """
     files = get_files_from_records(records)
-    s3 = boto3.client('s3')  # pylint: disable-msg=invalid-name
+    s3 = boto3.client("s3")  # pylint: disable-msg=invalid-name
     for attempt in range(1, max_retries + 1):
         for a_file in files:
             # All files from get_files_from_records start with 'success' == False.
             if not a_file[FILE_SUCCESS_KEY]:
-                err_msg = copy_object(s3,
-                                      a_file[INPUT_SOURCE_BUCKET_KEY],
-                                      a_file[INPUT_SOURCE_KEY_KEY],
-                                      a_file[INPUT_TARGET_BUCKET_KEY],
-                                      a_file[INPUT_TARGET_KEY_KEY])
+                err_msg = copy_object(
+                    s3,
+                    a_file[INPUT_SOURCE_BUCKET_KEY],
+                    a_file[INPUT_SOURCE_KEY_KEY],
+                    a_file[INPUT_TARGET_BUCKET_KEY],
+                    a_file[INPUT_TARGET_KEY_KEY],
+                )
                 if err_msg is None:
                     a_file[FILE_SUCCESS_KEY] = True
                     now = datetime.now(timezone.utc).isoformat()
                     shared_recovery.update_status_for_file(
-                        a_file[INPUT_JOB_ID_KEY], a_file[INPUT_GRANULE_ID_KEY], a_file[INPUT_FILENAME_KEY],
-                        shared_recovery.OrcaStatus.SUCCESS.value, None,
-                        db_queue_url
-                        )
+                        a_file[INPUT_JOB_ID_KEY],
+                        a_file[INPUT_GRANULE_ID_KEY],
+                        a_file[INPUT_FILENAME_KEY],
+                        shared_recovery.OrcaStatus.SUCCESS.value,
+                        None,
+                        db_queue_url,
+                    )
                 else:
                     a_file[FILE_ERROR_MESSAGE_KEY] = err_msg
 
         if attempt < max_retries + 1:  # Only sleep if not on the last attempt.
-            if all(a_file[FILE_SUCCESS_KEY] for a_file in files):  # Check for early completion
+            if all(
+                a_file[FILE_SUCCESS_KEY] for a_file in files
+            ):  # Check for early completion
                 break
             time.sleep(retry_sleep_secs)
 
@@ -87,14 +103,21 @@ def task(records: List[Dict[str, Any]], max_retries: int, retry_sleep_secs: floa
             any_error = True
             now = datetime.now(timezone.utc).isoformat()
             shared_recovery.update_status_for_file(
-                a_file[INPUT_JOB_ID_KEY], a_file[INPUT_GRANULE_ID_KEY], a_file[INPUT_FILENAME_KEY], 
-                shared_recovery.OrcaStatus.FAILED.value, a_file.get(FILE_ERROR_MESSAGE_KEY, None),db_queue_url)
+                a_file[INPUT_JOB_ID_KEY],
+                a_file[INPUT_GRANULE_ID_KEY],
+                a_file[INPUT_FILENAME_KEY],
+                shared_recovery.OrcaStatus.FAILED.value,
+                a_file.get(FILE_ERROR_MESSAGE_KEY, None),
+                db_queue_url,
+            )
     if any_error:
-        logging.error(
-            f'File copy failed. {files}')
-        raise CopyRequestError(f'File copy failed. {files}')
+        logging.error(f"File copy failed. {files}")
+        raise CopyRequestError(f"File copy failed. {files}")
 
-def get_files_from_records(records: List[Dict[str, Any]]) -> List[Dict[str, Union[str, bool]]]:
+
+def get_files_from_records(
+    records: List[Dict[str, Any]]
+) -> List[Dict[str, Union[str, bool]]]:
     """
     Parses the input records and returns the files to be restored.
     Args:
@@ -108,15 +131,20 @@ def get_files_from_records(records: List[Dict[str, Any]]) -> List[Dict[str, Unio
     validate = fastjsonschema.compile(schema)
     files = []
     for record in records:
-        a_file = json.loads(record['body'])
+        a_file = json.loads(record["body"])
         validate(a_file)
         a_file[FILE_SUCCESS_KEY] = False
         files.append(a_file)
     return files
 
 
-def copy_object(s3_cli: BaseClient, src_bucket_name: str, src_object_name: str,
-                dest_bucket_name: str, dest_object_name: str = None) -> Optional[str]:
+def copy_object(
+    s3_cli: BaseClient,
+    src_bucket_name: str,
+    src_object_name: str,
+    dest_bucket_name: str,
+    dest_object_name: str = None,
+) -> Optional[str]:
     """Copy an Amazon S3 bucket object
     Args:
         s3_cli: An instance of boto3 s3 client.
@@ -133,13 +161,13 @@ def copy_object(s3_cli: BaseClient, src_bucket_name: str, src_object_name: str,
     if dest_object_name is None:
         dest_object_name = src_object_name
     # Construct source bucket/object parameter
-    copy_source = {'Bucket': src_bucket_name, 'Key': src_object_name}
+    copy_source = {"Bucket": src_bucket_name, "Key": src_object_name}
 
     # Copy the object
     try:
-        response = s3_cli.copy_object(CopySource=copy_source,
-                                      Bucket=dest_bucket_name,
-                                      Key=dest_object_name)
+        response = s3_cli.copy_object(
+            CopySource=copy_source, Bucket=dest_bucket_name, Key=dest_object_name
+        )
         logging.debug(f"Copy response: {response}")
     except ClientError as ex:
         logging.error(ex)
@@ -148,7 +176,9 @@ def copy_object(s3_cli: BaseClient, src_bucket_name: str, src_object_name: str,
 
 
 # noinspection PyUnusedLocal
-def handler(event: Dict[str, Any], context: object) -> None:  # pylint: disable-msg=unused-argument
+def handler(
+    event: Dict[str, Any], context: object
+) -> None:  # pylint: disable-msg=unused-argument
     """Lambda handler. Copies a file from its temporary s3 bucket to the s3 archive.
     If the copy for a file in the request fails, the lambda
     throws an exception. Environment variables can be set to override how many
@@ -173,16 +203,17 @@ def handler(event: Dict[str, Any], context: object) -> None:  # pylint: disable-
         The same dict that is returned for a successful copy will be included in the
         message, with 'success' = False for the files for which the copy failed.
     """
-    logging.basicConfig(level=logging.INFO,
-                        format='%(levelname)s: %(asctime)s: %(message)s')
+    logging.basicConfig(
+        level=logging.INFO, format="%(levelname)s: %(asctime)s: %(message)s"
+    )
     try:
-        str_env_val = os.environ['COPY_RETRIES']
+        str_env_val = os.environ["COPY_RETRIES"]
         retries = int(str_env_val)
     except KeyError:
         retries = 2
 
     try:
-        str_env_val = os.environ['COPY_RETRY_SLEEP_SECS']
+        str_env_val = os.environ["COPY_RETRY_SLEEP_SECS"]
         retry_sleep_secs = float(str_env_val)
     except KeyError:
         retry_sleep_secs = 30
@@ -192,7 +223,7 @@ def handler(event: Dict[str, Any], context: object) -> None:  # pylint: disable-
     except KeyError as key_error:
         logging.error(f"os.environ[{OS_ENVIRON_DB_QUEUE_URL_KEY}] not found.")
         raise key_error
-    logging.debug(f'event: {event}')
+    logging.debug(f"event: {event}")
     records = event["Records"]
 
     task(records, retries, retry_sleep_secs, db_queue_url)
