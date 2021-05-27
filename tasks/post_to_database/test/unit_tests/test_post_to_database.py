59913b69e50f997b0c99ebd1caa5978c8b40fec2
Dorn <adorn@contractor.usgs.gov>
2021-05-27 10:23:51 -0500

    Update test_post_to_database.py
    Added more tests with better coverage of multiple values

--- tasks/post_to_database/test/unit_tests/test_post_to_database.py
+++ tasks/post_to_database/test/unit_tests/test_post_to_database.py
@@ -9,6 +9,8 @@ import unittest
 import uuid
 from unittest.mock import Mock, call, patch, MagicMock
 
+from fastjsonschema import JsonSchemaValueException
+
 import post_to_database
 from orca_shared.shared_recovery import OrcaStatus
 
@@ -82,6 +84,47 @@ class TestPostToDatabase(unittest.TestCase):  # pylint: disable-msg=too-many-ins
         mock_create_status_for_job_and_files.assert_called_once_with(job_id, granule_id, request_time,
                                                                      archive_destination, files, mock_engine)
 
+    def test_send_record_to_database_create_status_for_job_and_files_errors_for_missing_properties(
+            self):
+        """
+        No missing properties should be allowed
+        """
+        job_id = uuid.uuid4().__str__()
+        granule_id = uuid.uuid4().__str__()
+        request_time = datetime.datetime.utcnow().isoformat().__str__()
+        archive_destination = uuid.uuid4().__str__()
+        files = [{
+            'filename': uuid.uuid4().__str__(), 'key_path': uuid.uuid4().__str__(),
+            'restore_destination': uuid.uuid4().__str__(), 'status_id': 1,
+            'request_time': datetime.datetime.utcnow().isoformat(),
+            'last_update': datetime.datetime.utcnow().isoformat()
+        }]
+        values = {
+            "job_id": job_id,
+            "granule_id": granule_id,
+            "request_time": request_time,
+            "archive_destination": archive_destination,
+            "files": files
+        }
+        request_method = post_to_database.RequestMethod.NEW_JOB
+        mock_engine = Mock()
+
+        for key in values.keys():
+            input_values = values.copy()
+            input_values.pop(key)
+            record = {
+                'body': json.dumps(input_values, indent=4),
+                'messageAttributes': {'RequestMethod': request_method.value.__str__()}
+            }
+            schema_error_raised = False
+            try:
+                post_to_database.send_record_to_database(record, mock_engine)
+            except JsonSchemaValueException:
+                schema_error_raised = True
+
+            if not schema_error_raised:
+                self.fail(f"Key '{key}' schema_error_raised was '{schema_error_raised}'.")
+
     @patch('post_to_database.update_status_for_file')
     def test_send_record_to_database_update_status_for_file(self,
                                                             mock_update_status_for_file: MagicMock):
@@ -120,29 +163,54 @@ class TestPostToDatabase(unittest.TestCase):  # pylint: disable-msg=too-many-ins
         """
         Missing completion time and error_message are fine.
         """
+        expected_defaults = {'completion_time': None, 'error_message': None}
+
         job_id = uuid.uuid4().__str__()
         granule_id = uuid.uuid4().__str__()
         filename = uuid.uuid4().__str__()
         last_update = datetime.datetime.utcnow().isoformat().__str__()
         status_id = 3
+        completion_time = datetime.datetime.utcnow().isoformat().__str__()
+        error_message = uuid.uuid4().__str__()
         values = {
             "job_id": job_id,
             "granule_id": granule_id,
             "filename": filename,
             "last_update": last_update,
-            "status_id": status_id
+            "status_id": status_id,
+            "completion_time": completion_time,
+            "error_message": error_message
         }
         request_method = post_to_database.RequestMethod.UPDATE_FILE
         mock_engine = Mock()
-        record = {
-            'body': json.dumps(values, indent=4),
-            'messageAttributes': {'RequestMethod': request_method.value.__str__()}
-        }
 
-        post_to_database.send_record_to_database(record, mock_engine)
+        for key in values.keys():
+            input_values = values.copy()
+            input_values.pop(key)
+            record = {
+                'body': json.dumps(input_values, indent=4),
+                'messageAttributes': {'RequestMethod': request_method.value.__str__()}
+            }
+            schema_error_raised = False
+            try:
+                post_to_database.send_record_to_database(record, mock_engine)
+            except JsonSchemaValueException:
+                schema_error_raised = True
 
-        mock_update_status_for_file.assert_called_once_with(job_id, granule_id, filename, last_update, None,
-                                                            status_id, None, mock_engine)
+            if schema_error_raised == expected_defaults.keys().__contains__(key):
+                self.fail(f"Key '{key}' schema_error_raised was '{schema_error_raised}'.")
+            if expected_defaults.keys().__contains__(key):
+                expected_values = values.copy()
+                # noinspection PyTypeChecker
+                expected_values[key] = expected_defaults[key]
+                expected_values['engine'] = mock_engine
+                mock_update_status_for_file.assert_has_calls([call(expected_values['job_id'],
+                                                                   expected_values['granule_id'],
+                                                                   expected_values['filename'],
+                                                                   expected_values['last_update'],
+                                                                   expected_values['completion_time'],
+                                                                   expected_values['status_id'],
+                                                                   expected_values['error_message'], mock_engine)])
 
     @patch('post_to_database.create_file_sql')
     @patch('post_to_database.create_job_sql')
@@ -339,6 +407,43 @@ class TestPostToDatabase(unittest.TestCase):  # pylint: disable-msg=too-many-ins
             )
         ])
         self.assertEqual(2, mock_connection.execute.call_count)
+
+    def test_create_status_for_job_and_files_bad_status_id(self):
+        """
+        Only 'PENDING' and 'FAILED' should be allowed on initial creation.
+        """
+        job_id = uuid.uuid4().__str__()
+        granule_id = uuid.uuid4().__str__()
+        request_time = uuid.uuid4().__str__()
+        archive_destination = uuid.uuid4().__str__()
+
+        filename0 = uuid.uuid4().__str__()
+        key_path0 = uuid.uuid4().__str__()
+        restore_destination0 = uuid.uuid4().__str__()
+        request_time0 = uuid.uuid4().__str__()
+        last_update0 = uuid.uuid4().__str__()
+        error_message0 = uuid.uuid4().__str__()
+        completion_time0 = datetime.datetime.utcnow().isoformat()
+
+        for status in OrcaStatus:
+            if status == OrcaStatus.FAILED or status == OrcaStatus.PENDING:
+                continue
+            files = [
+                {
+                    'status_id': OrcaStatus.STAGED.value,
+                    'filename': filename0,
+                    'key_path': key_path0,
+                    'restore_destination': restore_destination0,
+                    'request_time': request_time0,
+                    'last_update': last_update0,
+                    'error_message': error_message0,
+                    'completion_time': completion_time0
+                }
+            ]
+
+            with self.assertRaises(ValueError):
+                post_to_database.create_status_for_job_and_files(job_id, granule_id, request_time, archive_destination,
+                                                                 files, Mock())
 
     @patch('post_to_database.update_file_sql')
     @patch('post_to_database.update_job_sql')
