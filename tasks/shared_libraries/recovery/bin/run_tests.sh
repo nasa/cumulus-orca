8dfa9818381d907c72a4ce66001fbb0bd22a2553
Dorn <adorn@contractor.usgs.gov>
2021-05-27 13:54:21 -0500

    Removed copy-pasted code that didn't work from shared lib test script.

--- tasks/shared_libraries/recovery/bin/run_tests.sh
+++ tasks/shared_libraries/recovery/bin/run_tests.sh
@@ -67,24 +67,6 @@ let return_code=$?
 
 check_rc $return_code "ERROR: pip install encountered an error."
 
-## Copy shared libraries
-echo "INFO: Copying ORCA shared libraries ..."
-if [ -d orca_shared ]; then
-    rm -rf orca_shared
-fi
-
-mkdir orca_shared
-let return_code=$?
-check_rc $return_code "ERROR: Unable to create orca_shared directory."
-
-touch orca_shared/__init__.py
-let return_code=$?
-check_rc $return_code "ERROR: Unable to create [orca_shared/__init__.py] file"
-
-cp ../shared_libraries/orca_shared/shared_recovery.py orca_shared/
-let return_code=$?
-check_rc $return_code "ERROR: Unable to copy shared library [orca_shared/shared_recovery.py]"
-
 ## Run unit tests and check Coverage
 echo "INFO: Running unit and coverage tests ....................."
 
