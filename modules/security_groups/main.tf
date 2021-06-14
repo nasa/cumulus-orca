34336b91c5a853863d82f7920123c31f43c51150
Bradley Hazuka <bhazuka@contractor.usgs.gov>
2021-06-14 12:52:29 -0500

    Fixed issue with security group lacking egress rule. This prevented Lambdas from retrieving secrets.

--- modules/security_groups/main.tf
+++ modules/security_groups/main.tf
@@ -40,4 +40,12 @@ resource "aws_security_group" "vpc-postgres-ingress-all-egress" {
     self      = true
   }
 
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
 }
