cb0a03abb3dc0fe34a504e797204c1fbf9c8edd5
Dorn <adorn@contractor.usgs.gov>
2021-06-15 14:29:29 -0500

    Better integration with CMA

--- tasks/copy_to_glacier_cumulus_translator/copy_to_glacier_cumulus_translator.py
+++ tasks/copy_to_glacier_cumulus_translator/copy_to_glacier_cumulus_translator.py
@@ -12,11 +12,12 @@ class ReformatRequestError(Exception):
     """
 
 
-def task(event: Dict[str, Any]) -> List[Dict[str, Any]]:
+def task(event: Dict[str, Any], context) -> List[Dict[str, Any]]:
     """
     Transforms event from Cumulus format to Orca copy_to_glacier format.
     Args:
         event: See schemas/input.json
+        context: An object passed through by CMA. Unused.
 
     Returns:
         See schemas/output.json
@@ -66,35 +67,10 @@ def handler(event: Dict[str, Any], context: Any) -> List[Dict[str, Any]]:
     """
     Entry point for the copy_to_glacier_cumulus_translator Lambda.
     Args:
-        event: See schemas/input.json
+        event: See schemas/input.json and combine with knowledge of CMA.
         context: An object provided by AWS Lambda. Used for context tracking.
 
-    Returns: A Dict with the following keys:
-        input (dict): Dictionary with the followig keys:
-            granules (List): List of dicts with the following keys:
-                granuleId (str):
-                files (List): List of dicts with the following keys:
-                    name (str):
-                    filepath (str):
-                    bucket (str):
-        config (dict): A dict with the following keys:
-            collection (dict): The collection from AWS.
-                See https://nasa.github.io/cumulus/docs/data-cookbooks/sips-workflow
-                A dict with the following keys:
-                name (str): The name of the collection.
-                    Used when generating the default value for {event}[config][fileStagingDir].
-                version (str): The version of the collection.
-                    Used when generating the default value for {event}[config][fileStagingDir].
-                files (list[Dict]): A list of dicts representing file types within the collection.
-                    The first file where the file's ['regex'] matches the filename from the input
-                    Is used to identify the bucket referenced in return's['granules'][filename]['files']['bucket']
-                    Each dict contains the following keys:
-                        regex (str): The regex that all files in the bucket must match with their name.
-                        bucket (str): The name of the bucket containing the files.
-                url_path (str): Used when calling {copy_granule_between_buckets} as a part of the destination_key.
-            buckets (dict): A dict with the following keys:
-                glacier (dict): A dict with the following keys:
-                    name (str): The name of the bucket to copy to.
+    Returns: See schemas/output.json
     """
     LOGGER.setMetadata(event, context)
     return run_cumulus_task.run_cumulus_task(task, event, context)
--- tasks/copy_to_glacier_cumulus_translator/test/unit_tests/test_copy_to_glacier_cumulus_translator_unit.py
+++ tasks/copy_to_glacier_cumulus_translator/test/unit_tests/test_copy_to_glacier_cumulus_translator_unit.py
@@ -20,9 +20,9 @@ class TestCopyToGlacierCumulusTranslatorUnit(
     @patch("run_cumulus_task.run_cumulus_task")
     @patch("cumulus_logger.CumulusLogger.setMetadata")
     def test_handler_happy_path(
-        self,
-        mock_setMetadata: MagicMock,
-        mock_task: MagicMock
+            self,
+            mock_setMetadata: MagicMock,
+            mock_run_cumulus_task: MagicMock
     ):
         """
         Basic path with all information present.
@@ -32,7 +32,45 @@ class TestCopyToGlacierCumulusTranslatorUnit(
         result = copy_to_glacier_cumulus_translator.handler(event, context)
 
         mock_setMetadata.assert_called_once_with(event, context)
-        mock_task.assert_called_once_with(
+        mock_run_cumulus_task.assert_called_once_with(
             copy_to_glacier_cumulus_translator.task, event, context
         )
-        self.assertEqual(mock_task.return_value, result)
+        self.assertEqual(mock_run_cumulus_task.return_value, result)
+
+    @patch("copy_to_glacier_cumulus_translator.task")
+    @patch("cumulus_logger.CumulusLogger.setMetadata")
+    def test_task_passthrough(self,
+                              mock_setMetadata: MagicMock,
+                              mock_task: MagicMock):
+        # event = {
+        #     "task_config": {
+        #         "file_mapping": {
+        #             "name": "some_name",
+        #             "filepath": "some/path",
+        #             "filename": "some/filename"
+        #         }
+        #     },
+        #     "payload": {
+        #         "granules": []
+        #     }
+        # }
+        expected_input = {"granules": []}
+        expected_config = {
+            "file_mapping": {
+                "name": "some_name",
+                "filepath": "some/path",
+                "bucket": "someBucket",
+                "filename": "some/filename"
+            }
+        }
+        event = {
+            "task_config": expected_config,
+            "payload": expected_input
+        }
+        context = Mock()
+
+        mock_task.return_value = {"granules": [{"granule_id": "some_granule_id", "files": []}]}
+
+        result = copy_to_glacier_cumulus_translator.handler(event, context)
+        self.assertEqual(mock_task.return_value, result['payload'])
+        mock_task.assert_called_once_with({"input": expected_input, "config": expected_config}, context)
