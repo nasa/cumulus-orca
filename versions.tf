68807e43b7465ca2ecb450308e1001d4999b6bd8
Dorn <adorn@contractor.usgs.gov>
2021-08-13 07:59:54 -0500

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,9 +1,10 @@
 ## Terraform Requirements
 terraform {
+  required_version = ">= 0.13"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.5.0"
+      version = ">= 3.14.1"
     }
   }
 }
