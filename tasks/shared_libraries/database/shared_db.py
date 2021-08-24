7c92fb1807eaa01e405ce7668be74ad8fb2e3ff7
Rizbi <rizbiece@gmail.com>
2021-08-24 14:36:15 +0000

    modified retry error type. Changed to psycopg2.errors.AdminShutdown

--- tasks/shared_libraries/database/shared_db.py
+++ tasks/shared_libraries/database/shared_db.py
@@ -10,11 +10,12 @@ import json
 import time
 import random
 import functools
-from sqlalchemy import create_engine, exc
+from sqlalchemy import create_engine
 from sqlalchemy.engine import URL
 from sqlalchemy.future import Engine
 from cumulus_logger import CumulusLogger
 from typing import Any, Dict, TypeVar, Callable
+import psycopg2
 
 # instantiate CumulusLogger
 logger = CumulusLogger(name="orca")
@@ -186,19 +187,19 @@ def retry_operational_error(max_retries: int = MAX_RETRIES, backoff_in_seconds: 
                 # Try the function and catch the expected error
                 try:
                     return func(*args, **kwargs)
-                except exc.OperationalError:
+                except psycopg2.errors.AdminShutdown:
                     if total_retries == max_retries:
                         # Log it and re-raise if we maxed our retries
-                        logger.error("Encountered  OperationalError {total_retries} times. Reached max retry limit.", total_retries = total_retries+1)
+                        logger.error("Encountered  AdminShutdown error {total_retries} times. Reached max retry limit.", total_retries = total_retries+1)
                         raise
                     else:
                         # perform exponential delay
                         backoff_time = (backoff_in_seconds * backoff_factor ** total_retries + random.uniform(0, 1))
-                        logger.error("Encountered OperationalError {total_retries} times. Sleeping {backoff_time} seconds.", total_retries=total_retries+1, backoff_time =backoff_time)
+                        logger.error("Encountered AdminShutdown {total_retries} times. Sleeping {backoff_time} seconds.", total_retries=total_retries+1, backoff_time =backoff_time)
                         time.sleep(backoff_time)
                         total_retries += 1
                 except Exception as ex:
-                    logger.error("Encountered a non retriable error {ex}", ex=ex)
+                    logger.error("Encountered a non retriable error: {ex}", ex=ex)
                     raise ex
     
         # Return our wrapper
