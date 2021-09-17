9d9b2b6bb16777e833aeb05c588430f8d198a52a
Bradley Hazuka <bhazuka@contractor.usgs.gov>
2021-09-17 13:06:05 -0500

    Adding initial chnages to copy_files_to_archive to get shared libraries from pip install.

--- tasks/copy_files_to_archive/bin/run_tests.sh
+++ tasks/copy_files_to_archive/bin/run_tests.sh
@@ -45,23 +45,7 @@ function check_rc () {
       exit 1
   fi
 }
-## copy the shared_recovery.py
-echo "INFO: Copying ORCA shared libraries ..."
-if [ -d orca_shared ]; then
-    rm -rf orca_shared
-fi
 
-mkdir orca_shared
-let return_code=$?
-check_rc $return_code "ERROR: Unable to create orca_shared directory."
-
-touch orca_shared/__init__.py
-let return_code=$?
-check_rc $return_code "ERROR: Unable to create [orca_shared/__init__.py] file"
-
-cp ../shared_libraries/recovery/shared_recovery.py orca_shared/
-let return_code=$?
-check_rc $return_code "ERROR: Unable to copy shared library [orca_shared/shared_recovery.py]"
 
 ## MAIN
 ## -----------------------------------------------------------------------------
