b53a89dfa26feb5f738b358fcba689db7ab628ef
Dorn <adorn@contractor.usgs.gov>
2021-11-02 15:12:31 -0500

    Switched request_status_for_job to new DB lib with retries

--- tasks/request_status_for_job/test/unit_tests/test_request_status_for_job_unit.py
+++ tasks/request_status_for_job/test/unit_tests/test_request_status_for_job_unit.py
@@ -3,16 +3,15 @@ Name: test_request_status_for_job_unit.py
 
 Description:  Unit tests for request_status_for_job.py.
 """
+import copy
 import json
 import unittest
 import uuid
 from http import HTTPStatus
 from unittest import mock
 from unittest.mock import patch, MagicMock, Mock
-
-import database
 import fastjsonschema as fastjsonschema
-import requests_db
+import sqlalchemy
 
 import request_status_for_job
 
@@ -26,7 +25,7 @@ class TestRequestStatusForJobUnit(
 
     # noinspection PyPep8Naming
     @patch("request_status_for_job.task")
-    @patch("requests_db.get_dbconnect_info")
+    @patch("orca_shared.database.shared_db.get_configuration")
     @patch("cumulus_logger.CumulusLogger.setMetadata")
     def test_handler_happy_path(
         self,
@@ -50,7 +49,7 @@ class TestRequestStatusForJobUnit(
         self.assertEqual(mock_task.return_value, result)
 
     # noinspection PyPep8Naming
-    @patch("requests_db.get_dbconnect_info")
+    @patch("orca_shared.database.shared_db.get_configuration")
     @patch("cumulus_logger.CumulusLogger.setMetadata")
     def test_handler_missing_job_id_returns_error_code(
         self, mock_setMetadata: MagicMock, mock_get_dbconnect_info: MagicMock
@@ -64,14 +63,16 @@ class TestRequestStatusForJobUnit(
         self.assertEqual(HTTPStatus.BAD_REQUEST, result["httpStatus"])
 
     # noinspection PyPep8Naming
+    @patch("cumulus_logger.CumulusLogger.error")
     @patch("request_status_for_job.task")
-    @patch("requests_db.get_dbconnect_info")
+    @patch("orca_shared.database.shared_db.get_configuration")
     @patch("cumulus_logger.CumulusLogger.setMetadata")
     def test_handler_database_error_returns_error_code(
         self,
         mock_setMetadata: MagicMock,
         mock_get_dbconnect_info: MagicMock,
         mock_task: MagicMock,
+        mock_cumulus_logger_error: MagicMock,
     ):
         """
         If database error is raised, it should be caught and returned in a dictionary.
@@ -80,16 +81,18 @@ class TestRequestStatusForJobUnit(
 
         event = {request_status_for_job.INPUT_JOB_ID_KEY: job_id}
         context = Mock()
-        mock_task.side_effect = requests_db.DatabaseError()
+        mock_task.side_effect = sqlalchemy.exc.OperationalError
         result = request_status_for_job.handler(event, context)
         self.assertEqual(HTTPStatus.INTERNAL_SERVER_ERROR, result["httpStatus"])
 
+    @patch("orca_shared.database.shared_db.get_user_connection")
     @patch("request_status_for_job.get_granule_status_entries_for_job")
     @patch("request_status_for_job.get_status_totals_for_job")
     def test_task_happy_path(
         self,
         mock_get_status_totals_for_job: MagicMock,
         mock_get_granule_status_entries_for_job: MagicMock,
+        mock_get_user_connection: MagicMock,
     ):
         """
         Basic path with all information present.
@@ -102,10 +105,13 @@ class TestRequestStatusForJobUnit(
 
         result = request_status_for_job.task(job_id, db_connect_info, request_id)
 
+        mock_get_user_connection.assert_called_once_with(db_connect_info)
         mock_get_granule_status_entries_for_job.assert_called_once_with(
-            job_id, db_connect_info
+            job_id, mock_get_user_connection.return_value
         )
-        mock_get_status_totals_for_job.assert_called_once_with(job_id, db_connect_info)
+        mock_get_status_totals_for_job.assert_called_once_with(
+            job_id, mock_get_user_connection.return_value
+        )
 
         self.assertEqual(
             {
@@ -116,12 +122,14 @@ class TestRequestStatusForJobUnit(
             result,
         )
 
+    @patch("orca_shared.database.shared_db.get_user_connection")
     @patch("request_status_for_job.create_http_error_dict")
     @patch("request_status_for_job.get_granule_status_entries_for_job")
     def test_task_no_granules_found_for_job_returns_error(
         self,
         mock_get_granule_status_entries_for_job: MagicMock,
         mock_create_http_error_dict: MagicMock,
+        mock_get_user_connection: MagicMock,
     ):
         """
         If no recovery_job entries exist for the given job_id, return 404.
@@ -132,8 +140,9 @@ class TestRequestStatusForJobUnit(
 
         result = request_status_for_job.task(job_id, db_connect_info, request_id)
 
+        mock_get_user_connection.assert_called_once_with(db_connect_info)
         mock_get_granule_status_entries_for_job.assert_called_once_with(
-            job_id, db_connect_info
+            job_id, mock_get_user_connection.return_value
         )
         mock_create_http_error_dict.assert_called_once_with(
             "NotFound", HTTPStatus.NOT_FOUND, request_id, mock.ANY
@@ -141,107 +150,67 @@ class TestRequestStatusForJobUnit(
 
         self.assertEqual(mock_create_http_error_dict.return_value, result)
 
-    @patch("database.result_to_json")
-    @patch("database.single_query")
-    def test_get_granule_status_entries_for_job_happy_path(
-        self, mock_single_query: MagicMock, mock_result_to_json: MagicMock
-    ):
+    @patch("request_status_for_job.get_granule_status_entries_for_job_sql")
+    def test_get_granule_status_entries_for_job_happy_path(self, mock_sql: MagicMock):
         """
         Basic path with all information present.
         """
         job_id = uuid.uuid4().__str__()
 
-        db_connect_info = Mock()
+        expected_result = [
+            {"granule_id": uuid.uuid4().__str__(), "status": uuid.uuid4().__str__()},
+            {"granule_id": uuid.uuid4().__str__(), "status": uuid.uuid4().__str__()},
+        ]
+        mock_engine = Mock()
+        mock_engine.begin.return_value = Mock()
+        mock_connection = Mock()
+        mock_connection.execute.return_value = copy.deepcopy(expected_result)
+        mock_engine.begin.return_value.__enter__ = Mock()
+        mock_engine.begin.return_value.__enter__.return_value = mock_connection
+        mock_engine.begin.return_value.__exit__ = (
+            Mock()
+        )  # required for "with", but untestable.
 
         result = request_status_for_job.get_granule_status_entries_for_job(
-            job_id, db_connect_info
+            job_id, mock_engine
         )
 
-        mock_single_query.assert_called_once_with(
-            f"""
-            SELECT
-                granule_id as "{request_status_for_job.OUTPUT_GRANULE_ID_KEY}",
-                recovery_status.value AS "{request_status_for_job.OUTPUT_STATUS_KEY}"
-            FROM
-                recovery_job
-            JOIN recovery_status ON recovery_job.status_id=recovery_status.id
-            WHERE
-                job_id = %s
-            """,
-            db_connect_info,
-            (job_id,),
+        mock_connection.execute.assert_called_once_with(
+            mock_sql.return_value,
+            [{"job_id": job_id}],
         )
-        mock_result_to_json.assert_called_once_with(mock_single_query.return_value)
 
-        self.assertEqual(mock_result_to_json.return_value, result)
+        self.assertEqual(expected_result, result)
 
-    @patch("database.single_query")
-    def test_get_granule_status_entries_for_job_error_wrapped_in_database_error(
-        self, mock_single_query: MagicMock
-    ):
-        """
-        If error is raised when contacting DB, error should be raised as a DatabaseError.
-        """
-        job_id = uuid.uuid4().__str__()
-        db_connect_info = Mock()
-
-        mock_single_query.side_effect = database.DbError()
-
-        with self.assertRaises(request_status_for_job.DatabaseError):
-            request_status_for_job.get_granule_status_entries_for_job(
-                job_id, db_connect_info
-            )
-
-    @patch("database.single_query")
-    def test_get_status_totals_for_job_happy_path(self, mock_single_query: MagicMock):
+    @patch("request_status_for_job.get_status_totals_for_job_sql")
+    def test_get_status_totals_for_job_happy_path(self, mock_sql: MagicMock):
         """
         Basic path with all information present.
         """
         job_id = uuid.uuid4().__str__()
-        db_connect_info = Mock()
 
-        mock_single_query.return_value = [
+        expected_result = [
             {"value": "success", "total": 5},
             {"value": "future_status", "total": 10},
         ]
+        mock_engine = Mock()
+        mock_engine.begin.return_value = Mock()
+        mock_connection = Mock()
+        mock_connection.execute.return_value = copy.deepcopy(expected_result)
+        mock_engine.begin.return_value.__enter__ = Mock()
+        mock_engine.begin.return_value.__enter__.return_value = mock_connection
+        mock_engine.begin.return_value.__exit__ = (
+            Mock()
+        )  # required for "with", but untestable.
 
-        result = request_status_for_job.get_status_totals_for_job(
-            job_id, db_connect_info
-        )
+        result = request_status_for_job.get_status_totals_for_job(job_id, mock_engine)
 
-        mock_single_query.assert_called_once_with(
-            f"""
-            with granule_status_count AS (
-                SELECT status_id
-                    , count(*) as total
-                FROM recovery_job
-                WHERE job_id = %s
-                GROUP BY status_id
-            )
-            SELECT value
-                , coalesce(total, 0) as total
-            FROM recovery_status os
-            LEFT JOIN granule_status_count gsc ON (gsc.status_id = os.id)""",
-            db_connect_info,
-            (job_id,),
+        mock_connection.execute.assert_called_once_with(
+            mock_sql.return_value,
+            [{"job_id": job_id}],
         )
 
         self.assertEqual({"success": 5, "future_status": 10}, result)
-
-    @patch("database.single_query")
-    def test_get_status_totals_for_job_error_wrapped_in_database_error(
-        self, mock_single_query: MagicMock
-    ):
-        """
-        If error is raised when contacting DB, error should be raised as a DatabaseError.
-        """
-        job_id = uuid.uuid4().__str__()
-        db_connect_info = Mock()
-
-        mock_single_query.side_effect = database.DbError()
-
-        with self.assertRaises(request_status_for_job.DatabaseError):
-            request_status_for_job.get_status_totals_for_job(job_id, db_connect_info)
 
     def test_task_job_id_cannot_be_none(self):
         """
@@ -254,8 +223,8 @@ class TestRequestStatusForJobUnit(
         self.fail("Error not raised.")
 
     # Multi-Function Tests:
-    @patch("database.single_query")
-    def test_task_output_json_schema(self, mock_single_query: MagicMock):
+    @patch("orca_shared.database.shared_db.get_user_connection")
+    def test_task_output_json_schema(self, mock_get_user_connection: MagicMock):
         """
         Checks a realistic output against the output.json.
         """
@@ -264,7 +233,10 @@ class TestRequestStatusForJobUnit(
 
         db_connect_info = Mock()
 
-        mock_single_query.side_effect = [
+        mock_engine = Mock()
+        mock_engine.begin.return_value = Mock()
+        mock_connection = Mock()
+        mock_connection.execute.side_effect = [
             # granules
             [
                 {
@@ -284,9 +256,16 @@ class TestRequestStatusForJobUnit(
                 {"value": "failed", "total": 1000},
             ],
         ]
+        mock_engine.begin.return_value.__enter__ = Mock()
+        mock_engine.begin.return_value.__enter__.return_value = mock_connection
+        mock_engine.begin.return_value.__exit__ = (
+            Mock()
+        )  # required for "with", but untestable.
+        mock_get_user_connection.return_value = mock_engine
 
         result = request_status_for_job.task(job_id, db_connect_info, request_id)
 
+        mock_get_user_connection.assert_called_once_with(db_connect_info)
         with open("schemas/output.json", "r") as raw_schema:
             schema = json.loads(raw_schema.read())
 
