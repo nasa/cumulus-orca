06d16213463acc3a351ec773bde14da7ee6095fa
Rizbi Hassan <rizbiece@gmail.com>
2022-01-07 10:40:52 -0600

    added terraform code for DLQ and cloudwatch alarm

--- modules/sqs/main.tf
+++ modules/sqs/main.tf
@@ -53,38 +53,40 @@ resource "aws_sqs_queue" "metadata_queue" {
 ## ====================================================================================================
 resource "aws_sqs_queue" "staged_recovery_queue" {
   ## OPTIONAL
-  name                        = "${var.prefix}-staged-recovery-queue"
-  delay_seconds               = var.sqs_delay_time_seconds
-  max_message_size            = var.sqs_maximum_message_size
-  message_retention_seconds   = var.staged_recovery_queue_message_retention_time_seconds
-  tags                        = local.tags
-  policy                      = data.aws_iam_policy_document.staged_recovery_queue_policy.json
-  visibility_timeout_seconds  = 1800 # Set to double lambda max time
+  name                       = "${var.prefix}-staged-recovery-queue"
+  delay_seconds              = var.sqs_delay_time_seconds
+  max_message_size           = var.sqs_maximum_message_size
+  message_retention_seconds  = var.staged_recovery_queue_message_retention_time_seconds
+  tags                       = local.tags
+  policy                     = data.aws_iam_policy_document.staged_recovery_queue_policy.json
+  visibility_timeout_seconds = 1800 # Set to double lambda max time
+  depends_on = [
+    aws_sqs_queue.staged_recovery_deadletter_queue
+  ]
   redrive_policy = jsonencode({
-    deadLetterTargetArn = aws_sqs_queue.deadletter_queue.arn,
-    maxReceiveCount     = 3
+    deadLetterTargetArn = aws_sqs_queue.staged_recovery_deadletter_queue.arn,
+    maxReceiveCount     = 3 #if number of delayed messages in the queue is greater than this number, then the messages will be sent to its DLQ.
   })
 }
 
-resource "aws_sqs_queue" "deadletter_queue" {
-  name                        = "${var.prefix}-staged-recovery-deadletter-queue"
-  delay_seconds               = var.sqs_delay_time_seconds
-  max_message_size            = var.sqs_maximum_message_size
-  message_retention_seconds   = var.staged_recovery_queue_message_retention_time_seconds
-  tags                        = local.tags
-  policy                      = data.aws_iam_policy_document.staged_recovery_queue_policy.json
-  visibility_timeout_seconds  = 1800 # Set to double lambda max time
+resource "aws_sqs_queue" "staged_recovery_deadletter_queue" {
+  name                       = "${var.prefix}-staged-recovery-deadletter-queue"
+  delay_seconds              = var.sqs_delay_time_seconds
+  max_message_size           = var.sqs_maximum_message_size
+  message_retention_seconds  = var.staged_recovery_queue_message_retention_time_seconds
+  tags                       = local.tags
+  visibility_timeout_seconds = 1800 # Set to same time as the staged recovery queue.
 }
 
-resource "aws_sqs_queue_policy" "deadletter_queue" {
-  queue_url = aws_sqs_queue.deadletter_queue.id
-  policy    = data.aws_iam_policy_document.deadletter_queue.json
+resource "aws_sqs_queue_policy" "deadletter_queue_policy" {
+  queue_url = aws_sqs_queue.staged_recovery_deadletter_queue.id
+  policy    = data.aws_iam_policy_document.staged_recovery_deadletter_queue_policy.json
 }
 
-data "aws_iam_policy_document" "deadletter_queue" {
+data "aws_iam_policy_document" "staged_recovery_deadletter_queue_policy" {
   statement {
     effect    = "Allow"
-    resources = [aws_sqs_queue.deadletter_queue.arn]
+    resources = [aws_sqs_queue.staged_recovery_deadletter_queue.arn]
     actions = [
       "sqs:ChangeMessageVisibility",
       "sqs:DeleteMessage",
@@ -97,6 +99,35 @@ data "aws_iam_policy_document" "deadletter_queue" {
   }
 }
 
+resource "aws_cloudwatch_metric_alarm" "staged_recovery_deadletter_alarm" {
+  alarm_name          = "${aws_sqs_queue.staged_recovery_deadletter_queue.name}-not-empty-alarm"
+  alarm_description   = "Items are on the ${aws_sqs_queue.staged_recovery_deadletter_queue.name} queue"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = 1
+  metric_name         = "ApproximateNumberOfMessagesVisible"
+  namespace           = "AWS/SQS"
+  period              = 300 #In seconds. Valid values for period are 1, 5, 10, 30, or any multiple of 60. 
+  statistic           = "Average"
+  threshold           = 1 #alarm will be triggered if number of messages in the DLQ equals to this threshold.
+  treat_missing_data  = "notBreaching"
+  alarm_actions       = [aws_sns_topic.dlq_alarm.arn]
+  tags                = local.tags
+  dimensions = {
+    "QueueName" = aws_sqs_queue.staged_recovery_deadletter_queue.name
+  }
+}
+
+# SNS topic needed for cloudwatch alarm
+resource "aws_sns_topic" "dlq_alarm" {
+  name = "dlq-alarm-topic"
+}
+
+resource "aws_sns_topic_subscription" "sns-subscription-for-alarm" {
+  depends_on = [aws_sns_topic.dlq_alarm]
+  topic_arn  = aws_sns_topic.dlq_alarm.arn
+  protocol   = "email"
+  endpoint   = "rhassan@contractor.usgs.gov"
+}
 ## status_update_queue - copy_files_to_archive lambda writes to this database status update queue.
 ## ===============================================================================================================================
 resource "aws_sqs_queue" "status_update_queue" {
