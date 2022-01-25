8812d7c577b25e357b17f4373f0abbf0863a1cdd
AndrewDorn <dornadigital@gmail.com>
2022-01-25 13:49:58 -0600

    Fixed naming and nullability

--- website/static/files/build_orca_reconcile.sql
+++ website/static/files/build_orca_reconcile.sql
@@ -28,8 +28,8 @@ BEGIN
   INSERT INTO recovery_status VALUES (5, 'success')
     ON CONFLICT (id) DO NOTHING;
 
-  -- Create reconcile_jobs table
-  CREATE TABLE IF NOT EXISTS reconcile_jobs  ( 
+  -- Create reconcile_job table
+  CREATE TABLE IF NOT EXISTS reconcile_job  ( 
     id                     	int8 NOT NULL
   , orca_archive_location  	text NOT NULL
   , status_id              	int2 NOT NULL
@@ -41,18 +41,18 @@ BEGIN
   , CONSTRAINT PK_reconcile_job PRIMARY KEY(id)
   , CONSTRAINT FK_reconcile_job_status FOREIGN KEY(status_id) REFERENCES reconcile_status(id)
   );
-  COMMENT ON TABLE reconcile_jobs IS 'Manages internal reconciliation job information.';
-  COMMENT ON COLUMN reconcile_jobs.id IS 'Job ID unique to each internal reconciliation job.';
-  COMMENT ON COLUMN reconcile_jobs.orca_archive_location IS 'ORCA S3 Glacier bucket the reconciliation targets.';
-  COMMENT ON COLUMN reconcile_jobs.status_id IS 'Current status of the job.';
-  COMMENT ON COLUMN reconcile_jobs.inventory_creation_time IS 'Inventory report creation time from the s3 manifest.';
-  COMMENT ON COLUMN reconcile_jobs.start_time IS 'Date and time the internal reconcile job started.';
-  COMMENT ON COLUMN reconcile_jobs.last_update IS 'Date and time the job status was last updated.';
-  COMMENT ON COLUMN reconcile_jobs.end_time IS 'Time the job completed and wrote the report information.';
-  COMMENT ON COLUMN reconcile_jobs.error_message IS 'Critical error the job ran into that prevented it from finishing.';
+  COMMENT ON TABLE reconcile_job IS 'Manages internal reconciliation job information.';
+  COMMENT ON COLUMN reconcile_job.id IS 'Job ID unique to each internal reconciliation job.';
+  COMMENT ON COLUMN reconcile_job.orca_archive_location IS 'ORCA S3 Glacier bucket the reconciliation targets.';
+  COMMENT ON COLUMN reconcile_job.status_id IS 'Current status of the job.';
+  COMMENT ON COLUMN reconcile_job.inventory_creation_time IS 'Inventory report creation time from the s3 manifest.';
+  COMMENT ON COLUMN reconcile_job.start_time IS 'Date and time the internal reconcile job started.';
+  COMMENT ON COLUMN reconcile_job.last_update IS 'Date and time the job status was last updated.';
+  COMMENT ON COLUMN reconcile_job.end_time IS 'Time the job completed and wrote the report information.';
+  COMMENT ON COLUMN reconcile_job.error_message IS 'Critical error the job ran into that prevented it from finishing.';
 
 
-  CREATE TABLE IF NOT EXISTS reconcile_s3_objects  ( 
+  CREATE TABLE IF NOT EXISTS reconcile_s3_object  ( 
     job_id               	int8 NOT NULL
   , orca_archive_location	text NOT NULL
   , key_path             	text NOT NULL
@@ -61,19 +61,19 @@ BEGIN
   , size_in_bytes        	int8 NOT NULL
   , storage_class        	text NOT NULL
   , delete_marker        	bool NOT NULL
-  , CONSTRAINT FK_reconcile_jobs_s3_objects FOREIGN KEY(job_id) REFERENCES reconcile_jobs(id)
-  , CONSTRAINT PK_reconcile_s3_objects PRIMARY KEY(orca_archive_location, key_path)
+  , CONSTRAINT FK_reconcile_job_s3_objects FOREIGN KEY(job_id) REFERENCES reconcile_job(id)
+  , CONSTRAINT PK_reconcile_s3_object PRIMARY KEY(orca_archive_location, key_path)
     ) PARTITION BY LIST (orca_archive_location);
-  COMMENT ON TABLE reconcile_s3_objects IS 'Temporary table that holds the listing from the ORCA S3 bucket to use for comparisons against the ORCA catalog.
+  COMMENT ON TABLE reconcile_s3_object IS 'Temporary table that holds the listing from the ORCA S3 bucket to use for comparisons against the ORCA catalog.
   Partitions should be placed around orca_archive_location.';
-  COMMENT ON COLUMN reconcile_s3_objects.job_id IS 'Job the S3 listing is a part of for the comparison. Foreign key to the reconcile jobs table.';
-  COMMENT ON COLUMN reconcile_s3_objects.orca_archive_location IS 'ORCA S3 Glacier bucket name where the file is stored.';
-  COMMENT ON COLUMN reconcile_s3_objects.key_path IS 'Full path and file name of the object in the S3 bucket.';
-  COMMENT ON COLUMN reconcile_s3_objects.etag IS 'AWS etag value from the s3 inventory report.';
-  COMMENT ON COLUMN reconcile_s3_objects.last_update IS 'AWS Last Update from the s3 inventory report.';
-  COMMENT ON COLUMN reconcile_s3_objects.size_in_bytes IS 'AWS size of the file in bytes from the s3 inventory report.';
-  COMMENT ON COLUMN reconcile_s3_objects.storage_class IS 'AWS storage class the object is in from the s3 inventory report.';
-  COMMENT ON COLUMN reconcile_s3_objects.delete_marker IS 'Set to `True` if object is a delete marker.';
+  COMMENT ON COLUMN reconcile_s3_object.job_id IS 'Job the S3 listing is a part of for the comparison. Foreign key to the reconcile jobs table.';
+  COMMENT ON COLUMN reconcile_s3_object.orca_archive_location IS 'ORCA S3 Glacier bucket name where the file is stored.';
+  COMMENT ON COLUMN reconcile_s3_object.key_path IS 'Full path and file name of the object in the S3 bucket.';
+  COMMENT ON COLUMN reconcile_s3_object.etag IS 'AWS etag value from the s3 inventory report.';
+  COMMENT ON COLUMN reconcile_s3_object.last_update IS 'AWS Last Update from the s3 inventory report.';
+  COMMENT ON COLUMN reconcile_s3_object.size_in_bytes IS 'AWS size of the file in bytes from the s3 inventory report.';
+  COMMENT ON COLUMN reconcile_s3_object.storage_class IS 'AWS storage class the object is in from the s3 inventory report.';
+  COMMENT ON COLUMN reconcile_s3_object.delete_marker IS 'Set to `True` if object is a delete marker.';
 
 
   CREATE TABLE IF NOT EXISTS reconcile_catalog_mismatch_report  ( 
@@ -91,7 +91,7 @@ BEGIN
   , s3_size                 	int8 NOT NULL
   , discrepancy_type        	text NOT NULL
   , CONSTRAINT PK_reconcile_catalog_mismatch_report PRIMARY KEY(job_id,collection_id,granule_id,key_path)
-  , CONSTRAINT FK_reconcile_jobs_mismatch_report FOREIGN KEY(job_id) REFERENCES reconcile_jobs(id)
+  , CONSTRAINT FK_reconcile_job_mismatch_report FOREIGN KEY(job_id) REFERENCES reconcile_job(id)
   );
   COMMENT ON TABLE reconcile_catalog_mismatch_report IS 'Table that identifies objects that have mismatched values between the Orca catalog and the s3 objects table.';
   COMMENT ON COLUMN reconcile_catalog_mismatch_report.job_id IS 'Job the mismatch or missing granule was found in. References the reconcile_job table.';
@@ -116,15 +116,15 @@ BEGIN
   , size_in_bytes	int8 NOT NULL
   , storage_class	text NOT NULL
   , CONSTRAINT PK_reconcile_orphan_report PRIMARY KEY(job_id,key_path)
-  , CONSTRAINT FK_reconcile_jobs_orphan_report FOREIGN KEY(job_id) REFERENCES reconcile_jobs(id)
+  , CONSTRAINT FK_reconcile_job_orphan_report FOREIGN KEY(job_id) REFERENCES reconcile_job(id)
   );
   COMMENT ON TABLE reconcile_orphan_report IS 'Table that identifies objects in the ORCA S3 Glacier bucket that are not in the ORCA catalog from the internal reconciliation job.';
   COMMENT ON COLUMN reconcile_orphan_report.job_id IS 'Associates the orphaned file to a internal reconciliation job. References the reconcile jobs table.';
-  COMMENT ON COLUMN reconcile_orphan_report.key_path IS 'Key that contains the path and file name. Value is obtained from the reconcile_s3_objects (key_path) column.';
-  COMMENT ON COLUMN reconcile_orphan_report.etag IS 'AWS Etag of the object. Value is obtained from the reconcile_s3_objects (etag) column.';
-  COMMENT ON COLUMN reconcile_orphan_report.last_update IS 'AWS last update of the object. Value is obtained from the reconcile_s3_objects (lst_update) column.';
-  COMMENT ON COLUMN reconcile_orphan_report.size_in_bytes IS 'AWS size of the object in bytes. Value is obtained from the reconcile_s3_objects (size) column.';
-  COMMENT ON COLUMN reconcile_orphan_report.storage_class IS 'AWS storage class the object is in. Value is obtained from the reconcile_s3_objects (storage_class) column.';
+  COMMENT ON COLUMN reconcile_orphan_report.key_path IS 'Key that contains the path and file name. Value is obtained from the reconcile_s3_object (key_path) column.';
+  COMMENT ON COLUMN reconcile_orphan_report.etag IS 'AWS Etag of the object. Value is obtained from the reconcile_s3_object (etag) column.';
+  COMMENT ON COLUMN reconcile_orphan_report.last_update IS 'AWS last update of the object. Value is obtained from the reconcile_s3_object (lst_update) column.';
+  COMMENT ON COLUMN reconcile_orphan_report.size_in_bytes IS 'AWS size of the object in bytes. Value is obtained from the reconcile_s3_object (size) column.';
+  COMMENT ON COLUMN reconcile_orphan_report.storage_class IS 'AWS storage class the object is in. Value is obtained from the reconcile_s3_object (storage_class) column.';
 
 
   CREATE TABLE IF NOT EXISTS reconcile_phantom_report  ( 
@@ -137,7 +137,7 @@ BEGIN
   , orca_last_update	timestamp with time zone NOT NULL
   , orca_size       	int8 NOT NULL
   , CONSTRAINT PK_reconcile_catalog_mismatch_report PRIMARY KEY(job_id,collection_id,granule_id,key_path)
-  , CONSTRAINT FK_reconcile_jobs_phantom_report FOREIGN KEY(job_id) REFERENCES reconcile_jobs(id)
+  , CONSTRAINT FK_reconcile_job_phantom_report FOREIGN KEY(job_id) REFERENCES reconcile_job(id)
   );
   COMMENT ON TABLE reconcile_phantom_report IS 'Table that identifies objects that exist in the ORCA catalog and do not exist in the ORCA S3 bucket.';
   COMMENT ON COLUMN reconcile_phantom_report.job_id IS 'Job the mismatch or missing granule was found in. References the reconcile_job table.';
