93d0c42b391868ff25de60dc0796dcc45b40523d
AndrewDorn <dornadigital@gmail.com>
2022-02-24 16:22:59 -0600

    perform_orca_reconcile functional SQL

--- tasks/perform_orca_reconcile/perform_orca_reconcile.py
+++ tasks/perform_orca_reconcile/perform_orca_reconcile.py
@@ -54,7 +54,7 @@ def task(
     user_engine = shared_db.get_user_connection(db_connect_info)
     orca_shared.reconciliation.shared_reconciliation.update_job(
         job_id,
-        orca_shared.reconciliation.OrcaStatus.GETTING_S3_LIST,
+        orca_shared.reconciliation.OrcaStatus.GENERATING_REPORTS,
         datetime.now(timezone.utc).isoformat(),
         None,
         user_engine,
@@ -90,7 +90,7 @@ def task(
 @shared_db.retry_operational_error()
 def generate_reports(job_id: int, orca_archive_location: str, engine: Engine) -> None:
     """
-    todo
+    Generates and posts phantom, orphan, and mismatch reports within the same transaction.
 
     Args:
         job_id: The id of the job containing s3 inventory info.
@@ -98,32 +98,184 @@ def generate_reports(job_id: int, orca_archive_location: str, engine: Engine) ->
         engine: The sqlalchemy engine to use for contacting the database.
     """
     try:
-        LOGGER.debug(f"Generating reports for job id {job_id}.")
+        LOGGER.debug(f"Generating phantom reports for job id {job_id}.")
         partition_name = get_partition_name_from_bucket_name(orca_archive_location)
-        # todo: populate reconcile_orphan_report with files in reconcile_s3_object but NOT orca.files
-        # todo: populate reconcile_phantom_report with files in orca.files but NOT reconcile_s3_object
-        # todo: populate reconcile_mismatch_report with files in both, but with a difference
         with engine.begin() as connection:
+            LOGGER.debug(f"Generating phantom reports for job id {job_id}.")
             connection.execute(
-                generate_reports_sql(partition_name),
-                [{}],
+                # populate reconcile_phantom_report with files in orca.files but NOT reconcile_s3_object
+                generate_phantom_reports_sql(partition_name),
+                [{"job_id": job_id, "orca_archive_location": orca_archive_location}],
             )
+            LOGGER.debug(f"Generating orphan reports for job id {job_id}.")
+            connection.execute(
+                # populate reconcile_orphan_report with files in reconcile_s3_object but NOT orca.files
+                generate_orphan_reports_sql(partition_name),
+                [{"job_id": job_id, "orca_archive_location": orca_archive_location}],
+            )
+            # populate reconcile_mismatch_report with files in both, but with a difference
+            generate_mismatch_reports(job_id, orca_archive_location, partition_name, connection)
     except Exception as sql_ex:
         LOGGER.error(f"Error while generating reports for job {job_id}: {sql_ex}")
         raise
 
 
-def generate_reports_sql(partition_name: str) -> TextClause:
+def generate_phantom_reports_sql(partition_name: str) -> TextClause:
     """
-    SQL for generating reports on differences between s3 and Orca catalog.
+    SQL for generating reports on files in the Orca catalog, but not S3.
     """
     return text(
-        # todo
         f"""
-        INSERT INTO orca.{partition_name} (job_id, orca_archive_location, key_path, etag, last_update, size_in_bytes, storage_class, delete_flag)
-            SELECT :job_id, orca_archive_location, key_path, etag, last_update, size_in_bytes, storage_class, delete_flag
-            FROM s3_import
-            WHERE is_latest = TRUE
+        WITH 
+            phantom_files AS (
+                SELECT files.*
+                FROM
+                    files
+                LEFT OUTER JOIN {partition_name} USING (key_path)
+                WHERE
+                    files.orca_archive_location = :orca_archive_location AND
+                    {partition_name}.key_path IS NULL
+            ),
+            phantom_reports AS (SELECT * FROM phantom_files
+            INNER JOIN granules ON (phantom_files.granule_id=granules.id))
+        INSERT INTO reconcile_phantom_report (job_id, collection_id, granule_id, filename, key_path, orca_etag, orca_last_update, orca_size)
+            SELECT :job_id, collection_id, cumulus_granule_id, name, key_path, etag, last_update, size_in_bytes
+            FROM phantom_reports"""
+    )
+
+
+def generate_orphan_reports_sql(partition_name: str) -> TextClause:
+    """
+    SQL for generating reports on files in S3, but not the Orca catalog.
+    """
+    return text(
+        f"""
+        WITH 
+            orphan_reports AS (
+                SELECT {partition_name}.*
+                FROM
+                    {partition_name}
+                LEFT OUTER JOIN files USING (key_path)
+                WHERE
+                    files.orca_archive_location = :orca_archive_location AND
+                    files.key_path IS NULL
+            )
+        INSERT INTO reconcile_orphan_report (job_id, key_path, etag, last_update, size_in_bytes, storage_class)
+            SELECT :job_id, key_path, etag, last_update, size_in_bytes, storage_class
+            FROM orphan_reports"""
+    )
+
+
+PAGE_SIZE = 500
+discrepancy_checks = ["etag", "last_update", "size_in_bytes"]
+def generate_mismatch_reports(job_id: int, orca_archive_location: str, partition_name: str, connection):
+    """
+    Generates and posts phantom, orphan, and mismatch reports within the same transaction.
+
+    Args:
+        job_id: The id of the job containing s3 inventory info.
+        orca_archive_location: The name of the bucket to generate the reports for.
+        partition_name: The name of the partition to retrieve s3 information from.
+        connection: The sqlalchemy connection to use for contacting the database.
+    """
+    another_page = True  # Indicates if there may be another page in Postgres
+    page_index = 0
+    while another_page:
+        LOGGER.debug(
+            f"Generating mismatch reports for job id {job_id}, page {page_index}."
+        )
+        mismatches = connection.execute(
+            get_mismatches_sql(partition_name),
+            [
+                {
+                    "orca_archive_location": orca_archive_location,
+                    "page_index": page_index,
+                    "page_size": PAGE_SIZE,
+                }
+            ],
+        )
+        another_page = False
+        mismatch_insert_params = []
+        for mismatch in mismatches:
+            discrepancies = []
+            for discrepancy_check in discrepancy_checks:
+                if (
+                        mismatch[f"s3_{discrepancy_check}"]
+                        != mismatch[f"orca_{discrepancy_check}"]
+                ):
+                    discrepancies.append(discrepancy_check)
+            params = {
+                "job_id": job_id,
+                "collection_id": mismatch["collection_id"],
+                "granule_id": mismatch["granule_id"],
+                "filename": mismatch["filename"],
+                "key_path": mismatch["key_path"],
+                "cumulus_archive_location": mismatch[
+                    "cumulus_archive_location"
+                ],
+                "orca_etag": mismatch["orca_etag"],
+                "s3_etag": mismatch["s3_etag"],
+                "orca_last_update": mismatch["orca_last_update"],
+                "s3_last_update": mismatch["s3_last_update"],
+                "orca_size_in_bytes": mismatch["orca_size_in_bytes"],
+                "s3_size_in_bytes": mismatch["s3_size_in_bytes"],
+                "discrepancy_type": ", ".join(discrepancies),
+            }
+            mismatch_insert_params.append(params)
+
+        if any(mismatch_insert_params):
+            if len(mismatch_insert_params) == PAGE_SIZE:
+                another_page = True
+            LOGGER.debug(
+                f"Inserting mismatch reports for job id {job_id}, page {page_index}."
+            )
+            connection.execute(insert_mismatch_sql(), mismatch_insert_params)
+        page_index = page_index + 1
+
+
+def get_mismatches_sql(partition_name: str) -> TextClause:
+    """
+    SQL for retrieving mismatches between entries in S3 and the Orca catalog.
+    """
+    return text(
+        f"""
+        SELECT
+            granules.collection_id, 
+            granules.cumulus_granule_id as granule_id, 
+            files.name as filename, 
+            files.key_path,
+             files.cumulus_archive_location, 
+            files.etag as orca_etag, 
+            {partition_name}.etag as s3_etag,
+            granules.last_update as orca_last_update,
+            {partition_name}.last_update as s3_last_update,
+            files.size_in_bytes as orca_size_in_bytes, 
+            {partition_name}.size_in_bytes as s3_size_in_bytes
+        FROM {partition_name}
+        INNER JOIN files USING (key_path)
+        INNER JOIN granules ON (files.granule_id=granules.id)
+        WHERE
+            files.orca_archive_location = :orca_archive_location
+            AND
+            (
+                files.etag != {partition_name}.etag OR
+                granules.last_update != {partition_name}.last_update OR
+                files.size_in_bytes != {partition_name}.size_in_bytes
+            )
+        LIMIT :page_size
+        OFFSET :page_index*:page_size"""
+    )
+
+
+def insert_mismatch_sql() -> TextClause:
+    """
+    SQL for posting a mismatch to reconcile_catalog_mismatch_report.
+    """
+    return text(
+        """
+        INSERT INTO reconcile_catalog_mismatch_report
+        VALUES(:job_id, :collection_id, :granule_id, :filename, :key_path, :cumulus_archive_location, :orca_etag, :s3_etag,
+            :orca_last_update, :s3_last_update, :orca_size_in_bytes, :s3_size_in_bytes, :discrepancy_type)
         """
     )
 
--- tasks/perform_orca_reconcile/test/unit_tests/test_perform_orca_reconcile.py
+++ tasks/perform_orca_reconcile/test/unit_tests/test_perform_orca_reconcile.py
@@ -2,6 +2,7 @@
 Name: test_perform_orca_reconcile.py
 Description:  Unit tests for test_perform_orca_reconcile.py.
 """
+import copy
 import datetime
 import json
 import random
@@ -12,8 +13,7 @@ from unittest.mock import Mock, call, patch, MagicMock
 from fastjsonschema import JsonSchemaValueException
 
 import perform_orca_reconcile
-from orca_shared.recovery.shared_recovery import OrcaStatus
-from orca_shared.recovery import shared_recovery
+from orca_shared.reconciliation import OrcaStatus
 
 
 class TestPerformOrcaReconcile(
@@ -22,3 +22,445 @@ class TestPerformOrcaReconcile(
     """
     TestPerformOrcaReconcile.
     """
+
+    @patch("orca_shared.reconciliation.shared_reconciliation.update_job")
+    @patch("perform_orca_reconcile.generate_reports")
+    @patch("orca_shared.database.shared_db.get_user_connection")
+    def test_task_happy_path(
+        self,
+        mock_get_user_connection: MagicMock,
+        mock_generate_reports: MagicMock,
+        mock_update_job: MagicMock,
+    ):
+        """
+        Happy path that triggers the various DB tasks.
+        """
+        db_connect_info = Mock()
+        mock_orca_archive_location = Mock()
+        mock_job_id = Mock()
+
+        result = perform_orca_reconcile.task(
+            mock_job_id,
+            mock_orca_archive_location,
+            db_connect_info,
+        )
+
+        mock_get_user_connection.assert_called_once_with(db_connect_info)
+        mock_generate_reports.assert_called_once_with(
+            mock_job_id,
+            mock_orca_archive_location,
+            mock_get_user_connection.return_value,
+        )
+        mock_update_job.assert_has_calls(
+            [
+                call(
+                    mock_job_id,
+                    OrcaStatus.GENERATING_REPORTS,
+                    unittest.mock.ANY,
+                    None,
+                    mock_get_user_connection.return_value,
+                    perform_orca_reconcile.LOGGER,
+                ),
+                call(
+                    mock_job_id,
+                    OrcaStatus.SUCCESS,
+                    unittest.mock.ANY,
+                    None,
+                    mock_get_user_connection.return_value,
+                    perform_orca_reconcile.LOGGER,
+                ),
+            ]
+        )
+
+        self.assertEqual(
+            {
+                perform_orca_reconcile.OUTPUT_JOB_ID_KEY: mock_job_id,
+            },
+            result,
+        )
+
+    @patch("perform_orca_reconcile.LOGGER")
+    @patch("orca_shared.reconciliation.shared_reconciliation.update_job")
+    @patch("perform_orca_reconcile.generate_reports")
+    @patch("orca_shared.database.shared_db.get_user_connection")
+    def test_task_error_posted_as_status_update(
+        self,
+        mock_get_user_connection: MagicMock,
+        mock_generate_reports: MagicMock,
+        mock_update_job: MagicMock,
+        mock_logger: MagicMock,
+    ):
+        """
+        Errors should be written to status entries.
+        """
+        expected_exception = Exception(uuid.uuid4().__str__())
+        mock_generate_reports.side_effect = expected_exception
+
+        db_connect_info = Mock()
+        mock_orca_archive_location = Mock()
+        mock_job_id = Mock()
+
+        with self.assertRaises(Exception) as cm:
+            perform_orca_reconcile.task(
+                mock_job_id,
+                mock_orca_archive_location,
+                db_connect_info,
+            )
+        self.assertEqual(expected_exception, cm.exception)
+
+        mock_get_user_connection.assert_called_once_with(db_connect_info)
+        mock_generate_reports.assert_called_once_with(
+            mock_job_id,
+            mock_orca_archive_location,
+            mock_get_user_connection.return_value,
+        )
+        mock_update_job.assert_has_calls(
+            [
+                call(
+                    mock_job_id,
+                    OrcaStatus.GENERATING_REPORTS,
+                    unittest.mock.ANY,
+                    None,
+                    mock_get_user_connection.return_value,
+                    perform_orca_reconcile.LOGGER,
+                ),
+                call(
+                    mock_job_id,
+                    OrcaStatus.ERROR,
+                    unittest.mock.ANY,
+                    str(expected_exception),
+                    mock_get_user_connection.return_value,
+                    mock_logger,
+                ),
+            ]
+        )
+        mock_logger.error.assert_called_once_with(
+            f"Encountered a fatal error: {expected_exception}"
+        )
+
+    @patch("perform_orca_reconcile.generate_mismatch_reports")
+    @patch("perform_orca_reconcile.generate_orphan_reports_sql")
+    @patch("perform_orca_reconcile.generate_phantom_reports_sql")
+    @patch("perform_orca_reconcile.get_partition_name_from_bucket_name")
+    def test_generate_reports_happy_path(
+        self,
+        mock_get_partition_name_from_bucket_name: MagicMock,
+        mock_generate_phantom_reports_sql: MagicMock,
+        mock_generate_orphan_reports_sql: MagicMock,
+        mock_generate_mismatch_reports: MagicMock,
+    ):
+        """
+        Happy path for generating reports in postgres
+        """
+        mock_job_id = Mock()
+        mock_orca_archive_location = Mock()
+        mock_execute = Mock()
+        mock_connection = Mock()
+        mock_connection.execute = mock_execute
+        mock_exit = Mock(return_value=False)
+        mock_enter = Mock()
+        mock_enter.__enter__ = Mock(return_value=mock_connection)
+        mock_enter.__exit__ = mock_exit
+        mock_engine = Mock()
+        mock_engine.begin = Mock(return_value=mock_enter)
+
+        perform_orca_reconcile.generate_reports(
+            mock_job_id, mock_orca_archive_location, mock_engine
+        )
+
+        mock_get_partition_name_from_bucket_name.assert_called_once_with(
+            mock_orca_archive_location
+        )
+        mock_enter.__enter__.assert_called_once_with()
+
+        mock_generate_phantom_reports_sql.assert_called_once_with(
+            mock_get_partition_name_from_bucket_name.return_value
+        )
+        mock_execute.assert_has_calls(
+            [
+                call(
+                    mock_generate_phantom_reports_sql.return_value,
+                    [
+                        {
+                            "job_id": mock_job_id,
+                            "orca_archive_location": mock_orca_archive_location,
+                        }
+                    ],
+                ),
+                call(
+                    mock_generate_orphan_reports_sql.return_value,
+                    [
+                        {
+                            "job_id": mock_job_id,
+                            "orca_archive_location": mock_orca_archive_location,
+                        }
+                    ],
+                ),
+            ]
+        )
+        self.assertEqual(2, mock_execute.call_count)
+
+        mock_generate_orphan_reports_sql.assert_called_once_with(
+            mock_get_partition_name_from_bucket_name.return_value
+        )
+
+        mock_generate_mismatch_reports.assert_called_once_with(
+            mock_job_id,
+            mock_orca_archive_location,
+            mock_get_partition_name_from_bucket_name.return_value,
+            mock_connection,
+        )
+
+        mock_exit.assert_called_once_with(None, None, None)
+
+    @patch("perform_orca_reconcile.LOGGER")
+    @patch("perform_orca_reconcile.generate_mismatch_reports")
+    @patch("perform_orca_reconcile.generate_orphan_reports_sql")
+    @patch("perform_orca_reconcile.generate_phantom_reports_sql")
+    @patch("perform_orca_reconcile.get_partition_name_from_bucket_name")
+    def test_generate_reports_error_logged_and_raised(
+        self,
+        mock_get_partition_name_from_bucket_name: MagicMock,
+        mock_generate_phantom_reports_sql: MagicMock,
+        mock_generate_orphan_reports_sql: MagicMock,
+        mock_generate_mismatch_reports: MagicMock,
+        mock_logger: MagicMock,
+    ):
+        """
+        Exceptions from Postgres should bubble up.
+        """
+        expected_exception = Exception(uuid.uuid4().__str__())
+        mock_job_id = Mock()
+        mock_orca_archive_location = Mock()
+        mock_execute = Mock(side_effect=expected_exception)
+        mock_connection = Mock()
+        mock_connection.execute = mock_execute
+        mock_exit = Mock(return_value=False)
+        mock_enter = Mock()
+        mock_enter.__enter__ = Mock(return_value=mock_connection)
+        mock_enter.__exit__ = mock_exit
+        mock_engine = Mock()
+        mock_engine.begin = Mock(return_value=mock_enter)
+
+        with self.assertRaises(Exception) as cm:
+            perform_orca_reconcile.generate_reports(
+                mock_job_id, mock_orca_archive_location, mock_engine
+            )
+        self.assertEqual(expected_exception, cm.exception)
+
+        mock_get_partition_name_from_bucket_name.assert_called_once_with(
+            mock_orca_archive_location
+        )
+        mock_enter.__enter__.assert_called_once_with()
+
+        mock_exit.assert_called_once_with(
+            Exception, expected_exception, unittest.mock.ANY
+        )
+        mock_logger.error.assert_called_once_with(
+            f"Error while generating reports for job {mock_job_id}: {expected_exception}"
+        )
+
+    @patch("perform_orca_reconcile.insert_mismatch_sql")
+    @patch("perform_orca_reconcile.get_mismatches_sql")
+    def test_generate_mismatch_reports_happy_path(
+        self,
+        mock_get_mismatches_sql: MagicMock,
+        mock_insert_mismatch_sql: MagicMock,
+    ):
+        """
+        Happy path for retrieving and writing mismatches to DB.
+        """
+        perform_orca_reconcile.PAGE_SIZE = len(
+            perform_orca_reconcile.discrepancy_checks
+        )
+        mock_job_id = Mock()
+        mock_orca_archive_location = Mock()
+        mock_partition_name = Mock()
+        mock_connection = Mock()
+        mismatch_rows = []
+        mismatch_insert_call_args = []
+        for discrepancy_check in perform_orca_reconcile.discrepancy_checks:
+            mock_etag = Mock()
+            mock_last_update = Mock()
+            mock_size_in_bytes = Mock()
+            mismatch = {
+                "collection_id": Mock(),
+                "granule_id": Mock(),
+                "filename": Mock(),
+                "key_path": Mock(),
+                "cumulus_archive_location": Mock(),
+                "orca_etag": mock_etag,
+                "s3_etag": mock_etag,
+                "orca_last_update": mock_last_update,
+                "s3_last_update": mock_last_update,
+                "orca_size_in_bytes": mock_size_in_bytes,
+                "s3_size_in_bytes": mock_size_in_bytes,
+                f"s3_{discrepancy_check}": Mock(),  # todo: does this work?
+            }
+            mismatch_rows.append(mismatch.copy())
+            mismatch["job_id"] = mock_job_id
+            mismatch["discrepancy_type"] = discrepancy_check
+            mismatch_insert_call_args.append(mismatch)
+        # todo: Add a mismatch of multiple types
+        mock_connection.execute.return_value = mismatch_rows
+
+        perform_orca_reconcile.generate_mismatch_reports(
+            mock_job_id,
+            mock_orca_archive_location,
+            mock_partition_name,
+            mock_connection,
+        )
+
+        mock_get_mismatches_sql.assert_called_once_with(mock_partition_name)
+        mock_connection.execute.assert_called_once_with(
+            mock_get_mismatches_sql.return_value,
+            [
+                {
+                    "orca_archive_location": mock_orca_archive_location,
+                    "page_index": 0,
+                    "page_size": perform_orca_reconcile.PAGE_SIZE,
+                }
+            ],
+        )
+
+    @patch("perform_orca_reconcile.get_mismatches_sql")
+    def test_generate_mismatch_reports_no_mismatches(
+        self,
+        mock_get_mismatches_sql: MagicMock,
+    ):
+        """
+        If no mismatches are found, do not write to DB.
+        """
+        mock_job_id = Mock()
+        mock_orca_archive_location = Mock()
+        mock_partition_name = Mock()
+        mock_connection = Mock()
+        mock_connection.execute.return_value = []
+
+        perform_orca_reconcile.generate_mismatch_reports(
+            mock_job_id,
+            mock_orca_archive_location,
+            mock_partition_name,
+            mock_connection,
+        )
+
+        mock_get_mismatches_sql.assert_called_once_with(mock_partition_name)
+        mock_connection.execute.assert_called_once_with(
+            mock_get_mismatches_sql.return_value,
+            [
+                {
+                    "orca_archive_location": mock_orca_archive_location,
+                    "page_index": 0,
+                    "page_size": perform_orca_reconcile.PAGE_SIZE,
+                }
+            ],
+        )
+
+    # noinspection PyPep8Naming
+    @patch("orca_shared.database.shared_db.get_configuration")
+    @patch("perform_orca_reconcile.LOGGER")
+    @patch("perform_orca_reconcile.task")
+    def test_handler_happy_path(
+        self,
+        mock_task: MagicMock,
+        mock_LOGGER: MagicMock,
+        mock_get_configuration: MagicMock,
+    ):
+        """
+        Happy path for handler assembling information to call Task.
+        """
+        job_id = random.randint(0, 1000)
+        orca_archive_location = uuid.uuid4().__str__()
+
+        expected_result = {
+            perform_orca_reconcile.OUTPUT_JOB_ID_KEY: random.randint(0, 1000),
+        }
+        mock_task.return_value = copy.deepcopy(expected_result)
+
+        mock_context = Mock()
+        event = {
+            perform_orca_reconcile.INPUT_JOB_ID_KEY: job_id,
+            perform_orca_reconcile.INPUT_ORCA_ARCHIVE_LOCATION_KEY: orca_archive_location,
+        }
+
+        result = perform_orca_reconcile.handler(event, mock_context)
+
+        mock_LOGGER.setMetadata.assert_called_once_with(event, mock_context)
+        mock_get_configuration.assert_called_once_with()
+        mock_task.assert_called_once_with(
+            job_id, orca_archive_location, mock_get_configuration.return_value
+        )
+        self.assertEqual(expected_result, result)
+
+    # noinspection PyPep8Naming
+    @patch("orca_shared.database.shared_db.get_configuration")
+    @patch("perform_orca_reconcile.LOGGER")
+    @patch("perform_orca_reconcile.task")
+    def test_handler_rejects_bad_input(
+        self,
+        mock_task: MagicMock,
+        mock_LOGGER: MagicMock,
+        mock_get_configuration: MagicMock,
+    ):
+        """
+        Violating input.json schema should raise an error.
+        """
+        job_id = random.randint(0, 1000)
+
+        expected_result = {
+            perform_orca_reconcile.OUTPUT_JOB_ID_KEY: random.randint(0, 1000),
+        }
+        mock_task.return_value = copy.deepcopy(expected_result)
+
+        mock_context = Mock()
+        event = {perform_orca_reconcile.INPUT_JOB_ID_KEY: job_id}
+
+        with self.assertRaises(Exception) as cm:
+            perform_orca_reconcile.handler(event, mock_context)
+
+        mock_LOGGER.setMetadata.assert_called_once_with(event, mock_context)
+        mock_task.assert_not_called()
+        self.assertEqual(
+            f"data must contain ['{perform_orca_reconcile.INPUT_JOB_ID_KEY}', '{perform_orca_reconcile.INPUT_ORCA_ARCHIVE_LOCATION_KEY}'] properties",
+            str(cm.exception),
+        )
+
+    # noinspection PyPep8Naming
+    @patch("orca_shared.database.shared_db.get_configuration")
+    @patch("perform_orca_reconcile.LOGGER")
+    @patch("perform_orca_reconcile.task")
+    def test_handler_rejects_bad_output(
+        self,
+        mock_task: MagicMock,
+        mock_LOGGER: MagicMock,
+        mock_get_configuration: MagicMock,
+    ):
+        """
+        Violating output.json schema should raise an error.
+        """
+        job_id = random.randint(0, 1000)
+        orca_archive_location = uuid.uuid4().__str__()
+
+        expected_result = {
+            perform_orca_reconcile.OUTPUT_JOB_ID_KEY: str(random.randint(0, 1000)),
+        }
+        mock_task.return_value = copy.deepcopy(expected_result)
+
+        mock_context = Mock()
+        event = {
+            perform_orca_reconcile.INPUT_JOB_ID_KEY: job_id,
+            perform_orca_reconcile.INPUT_ORCA_ARCHIVE_LOCATION_KEY: orca_archive_location,
+        }
+
+        with self.assertRaises(Exception) as cm:
+            perform_orca_reconcile.handler(event, mock_context)
+
+        mock_LOGGER.setMetadata.assert_called_once_with(event, mock_context)
+        mock_get_configuration.assert_called_once_with()
+        mock_task.assert_called_once_with(
+            job_id, orca_archive_location, mock_get_configuration.return_value
+        )
+        self.assertEqual(
+            f"data.{perform_orca_reconcile.INPUT_JOB_ID_KEY} must be integer",
+            str(cm.exception),
+        )
