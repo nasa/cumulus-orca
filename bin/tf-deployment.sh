92c88c9f64e3a0f0cb741fed04123c885b49571b
Rizbi Hassan <rizbiece@gmail.com>
2022-03-01 16:13:00 -0600

    aws configure added

--- bin/tf-deployment.sh
+++ bin/tf-deployment.sh
@@ -51,6 +51,10 @@ DATA_PERSISTENCE_KEY="$DEPLOYMENT/data-persistence-tf/terraform.tfstate"
 #     }
 # }" >> ci_backend.tf
 
+
+export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
+export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
+
 #configure aws 
 $ aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
 $ aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
