82578f8a90b7a7e794e5153d06a604d0dbb15807
Rizbi Hassan <rizbiece@gmail.com>
2022-03-02 11:20:13 -0600

    terraform file fix

--- bin/tf-deployment.sh
+++ bin/tf-deployment.sh
@@ -53,8 +53,7 @@ mv terraform.tf.example terraform.tf
 
 DATA_PERSISTENCE_KEY="$bamboo_PREFIX/data-persistence-tf/terraform.tfstate"
 # Ensure remote state is configured for the deployment
-echo 
-"terraform {
+echo "terraform {
         backend \"s3\" {
             bucket = \"$bamboo_TFSTATE_BUCKET\"
             key    = \"$DATA_PERSISTENCE_KEY\"
