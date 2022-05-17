5847d143f80eb191a5532c1df5f4a97040a2c35a
Rizbi Hassan <rizbiece@gmail.com>
2022-05-16 20:58:47 -0500

    new build

--- bin/deployment-cumulus-orca.sh
+++ bin/deployment-cumulus-orca.sh
@@ -233,7 +233,6 @@ terraform apply \
   -lock=false \
   -input=false \
   -var-file="terraform.tfvars" \
-  -var "cumulus_message_adapter_lambda_layer_version_arn=arn:aws:lambda:$bamboo_AWS_DEFAULT_REGION:$bamboo_AWS_ACCOUNT_ID:layer:Cumulus_Message_Adapter:4" \
   -var "cmr_username=$bamboo_CMR_USERNAME" \
   -var "cmr_password=$bamboo_CMR_PASSWORD" \
   -var "cmr_client_id=cumulus-core-$bamboo_DEPLOYMENT" \
