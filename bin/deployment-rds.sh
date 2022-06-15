57cff9c5f8f57831303e8727c77b09620b212220
Rizbi Hassan <rizbiece@gmail.com>
2022-06-15 15:56:29 -0500

    destroy resource

--- bin/deployment-rds.sh
+++ bin/deployment-rds.sh
@@ -27,7 +27,7 @@ resource "aws_s3_bucket" "orca-primary" {
   bucket = "PREFIX-orca-primary"
 }
 resource "aws_s3_bucket" "protected" {
-  bucket = "PREFIX-protected-test"
+  bucket = "PREFIX-protected"
 }
 resource "aws_dynamodb_table" "tf-locks" {
   name           = "PREFIX-tf-locks"
@@ -61,40 +61,40 @@ terraform apply \
 #copy the terraform state file to the created tf-state bucket
 aws s3 cp terraform.tfstate s3://$bamboo_PREFIX-tf-state/terraform.tfstate
 
-# #clone cumulus orca template for deploying cumulus and orca
-# git clone --branch $bamboo_CUMULUS_ORCA_DEPLOY_TEMPLATE_VERSION --single-branch https://git.earthdata.nasa.gov/scm/orca/cumulus-orca-deploy-template.git
-# cd cumulus-orca-deploy-template
-# echo "checked out to $bamboo_CUMULUS_ORCA_DEPLOY_TEMPLATE_VERSION branch"
+#clone cumulus orca template for deploying cumulus and orca
+git clone --branch $bamboo_CUMULUS_ORCA_DEPLOY_TEMPLATE_VERSION --single-branch https://git.earthdata.nasa.gov/scm/orca/cumulus-orca-deploy-template.git
+cd cumulus-orca-deploy-template
+echo "checked out to $bamboo_CUMULUS_ORCA_DEPLOY_TEMPLATE_VERSION branch"
 
 
-# #deploy rds-cluster-tf module
+#deploy rds-cluster-tf module
 
-# cd rds-cluster-tf
-# echo "inside rds-cluster-tf"
-# mv terraform.tfvars.example terraform.tfvars
+cd rds-cluster-tf
+echo "inside rds-cluster-tf"
+mv terraform.tfvars.example terraform.tfvars
 
-# #replacing terraform.tf with proper values
-# sed -e 's/PREFIX/'"$bamboo_PREFIX"'/g; s/us-east-1/'"$bamboo_AWS_DEFAULT_REGION"'/g' terraform.tf.example > terraform.tf
+#replacing terraform.tf with proper values
+sed -e 's/PREFIX/'"$bamboo_PREFIX"'/g; s/us-east-1/'"$bamboo_AWS_DEFAULT_REGION"'/g' terraform.tf.example > terraform.tf
 
-# # Initialize deployment
-# terraform init \
-#   -input=false
+# Initialize deployment
+terraform init \
+  -input=false
 
-# # Deploy drds-cluster-tf via terraform
-# echo "Deploying rds-cluster-tf  module to $bamboo_DEPLOYMENT"
-# terraform apply \
-#   -auto-approve \
-#   -lock=false \
-#   -input=false \
-#   -var-file="terraform.tfvars" \
-#   -var "prefix=$bamboo_PREFIX" \
-#   -var "region=$bamboo_AWS_DEFAULT_REGION" \
-#   -var "subnets=[\"$bamboo_AWS_SUBNET_ID1\", \"$bamboo_AWS_SUBNET_ID2\"]" \
-#   -var "db_admin_username=$bamboo_DB_ADMIN_USERNAME" \
-#   -var "db_admin_password=$bamboo_DB_ADMIN_PASSWORD" \
-#   -var "vpc_id=$bamboo_VPC_ID" \
-#   -var "cluster_identifier=$bamboo_PREFIX-cumulus-rds-serverless-default-cluster" \
-#   -var "deletion_protection=false"\
-#   -var "provision_user_database=false"\
-#   -var "engine_version=$bamboo_RDS_ENGINE_VERSION" \
-#   -var "permissions_boundary_arn=arn:aws:iam::$bamboo_AWS_ACCOUNT_ID:policy/$bamboo_ROLE_BOUNDARY"
+# Deploy drds-cluster-tf via terraform
+echo "Deploying rds-cluster-tf  module to $bamboo_DEPLOYMENT"
+terraform apply \
+  -auto-approve \
+  -lock=false \
+  -input=false \
+  -var-file="terraform.tfvars" \
+  -var "prefix=$bamboo_PREFIX" \
+  -var "region=$bamboo_AWS_DEFAULT_REGION" \
+  -var "subnets=[\"$bamboo_AWS_SUBNET_ID1\", \"$bamboo_AWS_SUBNET_ID2\"]" \
+  -var "db_admin_username=$bamboo_DB_ADMIN_USERNAME" \
+  -var "db_admin_password=$bamboo_DB_ADMIN_PASSWORD" \
+  -var "vpc_id=$bamboo_VPC_ID" \
+  -var "cluster_identifier=$bamboo_PREFIX-cumulus-rds-serverless-default-cluster" \
+  -var "deletion_protection=false"\
+  -var "provision_user_database=false"\
+  -var "engine_version=$bamboo_RDS_ENGINE_VERSION" \
+  -var "permissions_boundary_arn=arn:aws:iam::$bamboo_AWS_ACCOUNT_ID:policy/$bamboo_ROLE_BOUNDARY"
