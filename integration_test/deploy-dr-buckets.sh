51bd05154e515d0c6f21349df1f8db21dcffb182
Rizbi Hassan <rizbiece@gmail.com>
2022-07-11 10:57:13 -0500

    test

--- integration_test/deploy-dr-buckets.sh
+++ integration_test/deploy-dr-buckets.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -ex
 
-export AWS_ACCESS_KEY_ID=$bamboo_AWS_ACCESS_KEY_ID
-export AWS_SECRET_ACCESS_KEY=$bamboo_AWS_SECRET_ACCESS_KEY
+export DR_AWS_ACCESS_KEY_ID=$bamboo_DR_AWS_ACCESS_KEY_ID
+export DR_AWS_SECRET_ACCESS_KEY=$bamboo_DR_AWS_SECRET_ACCESS_KEY
 export AWS_DEFAULT_REGION=$bamboo_AWS_DEFAULT_REGION
 
 #remove old files from bamboo as they throw error
@@ -11,11 +11,27 @@ git clone --branch develop --single-branch https://github.com/nasa/cumulus-orca.
 #replace prefix with bamboo prefix variable
 sed -e 's/PREFIX/'"$bamboo_PREFIX"'/g' dr-buckets.tf.template > dr-buckets.tf
 
-if ! aws s3 cp s3://$bamboo_PREFIX-dr-tf-state/dr-buckets-tf/terraform.tfstate .;then
-    echo "terraform state file not present in S3. Creating the buckets in DR account."
+if ! aws s3api head-bucket --bucket $bamboo_PREFIX-tf-state;;then
+    echo "terraform state bucket is not created. Creating ..."
+    aws s3api create-bucket --bucket $bamboo_PREFIX-tf-state  --region $AWS_REGION --create-bucket-configuration LocationConstraint=$AWS_REGION
+    aws dynamodb create-table \
+      --table-name $bamboo_PREFIX-tf-locks \
+      --attribute-definitions AttributeName=LockID,AttributeType=S \
+      --key-schema AttributeName=LockID,KeyType=HASH \
+      --billing-mode PAY_PER_REQUEST \
+      --region $AWS_REGION
 else
-    echo "Terraform state file found in S3 bucket. Using S3 remote backend"
+    echo "terraform state bucket present."
 fi
+
+#configruing S3 backend
+echo "terraform {
+  backend \"s3\" {
+    bucket = \"$bamboo_PREFIX-tf-state\"
+    region = \"$AWS_REGION\"
+    dynamodb_table = \"$bamboo_PREFIX-tf-locks\"
+  }
+}"
 
 terraform init -input=false
 echo "Deploying S3  buckets in Disaaster Recovery account"
