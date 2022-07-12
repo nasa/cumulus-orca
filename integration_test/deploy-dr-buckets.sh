2e41ba94a67e3dfb3e385cf33e838a6e2cad71a7
Rizbi Hassan <rizbiece@gmail.com>
2022-07-12 15:41:48 -0500

    updated script for deploying buckets

--- integration_test/deploy-dr-buckets.sh
+++ integration_test/deploy-dr-buckets.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -ex
 
-export DR_AWS_ACCESS_KEY_ID=$bamboo_DR_AWS_ACCESS_KEY_ID
-export DR_AWS_SECRET_ACCESS_KEY=$bamboo_DR_AWS_SECRET_ACCESS_KEY
+export AWS_ACCESS_KEY_ID=$bamboo_DR_AWS_ACCESS_KEY_ID
+export AWS_SECRET_ACCESS_KEY=$bamboo_DR_AWS_SECRET_ACCESS_KEY
 export AWS_DEFAULT_REGION=$bamboo_AWS_DEFAULT_REGION
 
 #remove old files from bamboo as they throw error
@@ -11,17 +11,38 @@ git clone --branch develop --single-branch https://github.com/nasa/cumulus-orca.
 #replace prefix with bamboo prefix variable
 sed -e 's/PREFIX/'"$bamboo_PREFIX"'/g' dr-buckets.tf.template > dr-buckets.tf
 
-if ! aws s3 cp s3://$bamboo_PREFIX-dr-tf-state/dr-buckets-tf/terraform.tfstate .;then
-    echo "terraform state file not present in S3. Creating the buckets in DR account."
+if ! aws s3api head-bucket --bucket $bamboo_PREFIX-dr-tf-state;then
+    echo "terraform state bucket is not created. Creating ..."
+    aws s3api create-bucket --bucket $bamboo_PREFIX-dr-tf-state  --region $bamboo_AWS_DEFAULT_REGION --create-bucket-configuration LocationConstraint=$bamboo_AWS_DEFAULT_REGION
+    
+    aws s3api put-bucket-versioning \
+    --bucket $bamboo_PREFIX-dr-tf-state \
+    --versioning-configuration Status=Enabled
+
+    aws dynamodb create-table \
+      --table-name $bamboo_PREFIX-dr-tf-locks \
+      --attribute-definitions AttributeName=LockID,AttributeType=S \
+      --key-schema AttributeName=LockID,KeyType=HASH \
+      --billing-mode PAY_PER_REQUEST \
+      --region $bamboo_AWS_DEFAULT_REGION
 else
-    echo "Terraform state file found in S3 bucket. Using S3 remote backend"
+    echo "terraform state bucket already present. Using existing state file"
 fi
 
+#configuring S3 backend
+echo "terraform {
+  backend \"s3\" {
+    bucket = \"$bamboo_PREFIX-dr-tf-state\"
+    region = \"$bamboo_AWS_DEFAULT_REGION\"
+    key    = \"terraform.tfstate\"
+    dynamodb_table = \"$bamboo_PREFIX-dr-tf-locks\"
+  }
+}" >> terraform.tf
+
+#initialize terraform
 terraform init -input=false
+#deploy using terraform
 echo "Deploying S3  buckets in Disaaster Recovery account"
 terraform apply \
   -auto-approve \
   -input=false
-
-#copy terraform state file to the created tf-state bucket
-aws s3 cp terraform.tfstate s3://$bamboo_PREFIX-dr-tf-state/dr-buckets-tf/terraform.tfstate
