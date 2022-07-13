81c4b98e0c6c3e04c551f74860d0f9a84b1ab1c9
Rizbi Hassan <rizbiece@gmail.com>
2022-07-13 07:29:48 -0500

    test policy

--- integration_test/deploy-dr-buckets.sh
+++ integration_test/deploy-dr-buckets.sh
@@ -11,20 +11,20 @@ git clone --branch develop --single-branch https://github.com/nasa/cumulus-orca.
 #replace prefix with bamboo prefix variable
 sed -e 's/PREFIX/'"$bamboo_PREFIX"'/g' dr-buckets.tf.template > dr-buckets.tf
 
-if ! aws s3api head-bucket --bucket $bamboo_PREFIX-dr-tf-state;then
+if ! aws s3api head-bucket --bucket ${bamboo_PREFIX}-dr-tf-state;then
     echo "terraform state bucket is not created. Creating ..."
-    aws s3api create-bucket --bucket $bamboo_PREFIX-dr-tf-state  --region $bamboo_AWS_DEFAULT_REGION --create-bucket-configuration LocationConstraint=$bamboo_AWS_DEFAULT_REGION
+    aws s3api create-bucket --bucket ${bamboo_PREFIX}-dr-tf-state  --region ${bamboo_AWS_DEFAULT_REGION} --create-bucket-configuration LocationConstraint=${bamboo_AWS_DEFAULT_REGION}
     
     aws s3api put-bucket-versioning \
-    --bucket $bamboo_PREFIX-dr-tf-state \
+    --bucket ${bamboo_PREFIX}-dr-tf-state \
     --versioning-configuration Status=Enabled
 
     aws dynamodb create-table \
-      --table-name $bamboo_PREFIX-dr-tf-locks \
+      --table-name ${bamboo_PREFIX}-dr-tf-locks \
       --attribute-definitions AttributeName=LockID,AttributeType=S \
       --key-schema AttributeName=LockID,KeyType=HASH \
       --billing-mode PAY_PER_REQUEST \
-      --region $bamboo_AWS_DEFAULT_REGION
+      --region ${bamboo_AWS_DEFAULT_REGION}
 else
     echo "terraform state bucket present."
 fi
@@ -32,10 +32,10 @@ fi
 #configruing S3 backend
 echo "terraform {
   backend \"s3\" {
-    bucket = \"$bamboo_PREFIX-dr-tf-state\"
-    region = \"$bamboo_AWS_DEFAULT_REGION\"
+    bucket = \"${bamboo_PREFIX}-dr-tf-state\"
+    region = \"${bamboo_AWS_DEFAULT_REGION}\"
     key    = \"terraform.tfstate\"
-    dynamodb_table = \"$bamboo_PREFIX-dr-tf-locks\"
+    dynamodb_table = \"${bamboo_PREFIX}-dr-tf-locks\"
   }
 }" >> terraform.tf
 
