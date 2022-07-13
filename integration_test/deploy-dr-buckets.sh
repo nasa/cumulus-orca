0754b659c2ac398c41bb1b131eca2d06311450fd
Rizbi Hassan <rizbiece@gmail.com>
2022-07-13 09:19:40 -0500

    updated worm bucket policy, added checks for prefix variable

--- integration_test/deploy-dr-buckets.sh
+++ integration_test/deploy-dr-buckets.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 set -ex
+if [[ "${bamboo_PREFIX}" = 'REPLACE ME' ]]; then
+    echo "ERROR! You must override this variable with your prefix while running the pipeline"
+    exit 1
+fi
 
 export AWS_ACCESS_KEY_ID=$bamboo_DR_AWS_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY=$bamboo_DR_AWS_SECRET_ACCESS_KEY
@@ -11,20 +15,20 @@ git clone --branch develop --single-branch https://github.com/nasa/cumulus-orca.
 #replace prefix with bamboo prefix variable
 sed -e 's/PREFIX/'"$bamboo_PREFIX"'/g' dr-buckets.tf.template > dr-buckets.tf
 
-if ! aws s3api head-bucket --bucket $bamboo_PREFIX-dr-tf-state;then
+if ! aws s3api head-bucket --bucket ${bamboo_PREFIX}-dr-tf-state;then
     echo "terraform state bucket is not created. Creating ..."
-    aws s3api create-bucket --bucket $bamboo_PREFIX-dr-tf-state  --region $bamboo_AWS_DEFAULT_REGION --create-bucket-configuration LocationConstraint=$bamboo_AWS_DEFAULT_REGION
+    aws s3api create-bucket --bucket ${bamboo_PREFIX}-dr-tf-state  --region ${bamboo_AWS_DEFAULT_REGION} --create-bucket-configuration LocationConstraint=${bamboo_AWS_DEFAULT_REGION}
     
     aws s3api put-bucket-versioning \
-    --bucket $bamboo_PREFIX-dr-tf-state \
+    --bucket ${bamboo_PREFIX}-dr-tf-state \
     --versioning-configuration Status=Enabled
 
     aws dynamodb create-table \
-      --table-name $bamboo_PREFIX-dr-tf-locks \
+      --table-name ${bamboo_PREFIX}-dr-tf-locks \
       --attribute-definitions AttributeName=LockID,AttributeType=S \
       --key-schema AttributeName=LockID,KeyType=HASH \
       --billing-mode PAY_PER_REQUEST \
-      --region $bamboo_AWS_DEFAULT_REGION
+      --region ${bamboo_AWS_DEFAULT_REGION}
 else
     echo "terraform state bucket already present. Using existing state file"
 fi
@@ -32,10 +36,10 @@ fi
 #configuring S3 backend
 echo "terraform {
   backend \"s3\" {
-    bucket = \"$bamboo_PREFIX-dr-tf-state\"
-    region = \"$bamboo_AWS_DEFAULT_REGION\"
+    bucket = \"${bamboo_PREFIX}-dr-tf-state\"
+    region = \"${bamboo_AWS_DEFAULT_REGION}\"
     key    = \"terraform.tfstate\"
-    dynamodb_table = \"$bamboo_PREFIX-dr-tf-locks\"
+    dynamodb_table = \"${bamboo_PREFIX}-dr-tf-locks\"
   }
 }" >> terraform.tf
 
