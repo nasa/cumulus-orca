0562d0cf26790d757cd24178df668334558f4a9d
Rizbi Hassan <rizbiece@gmail.com>
2022-07-13 11:22:47 -0500

    test

--- integration_test/deploy-dr-buckets.sh
+++ integration_test/deploy-dr-buckets.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 # Negated REGEX that checks that the PREFIX is alpha numeric with no spaces and the optional use of an _
-if [[ ${bamboo_PREFIX} != ^[^a-zA-Z0-9_]+$ ]]; then
+if ! [[ ${bamboo_PREFIX} =~ ^[^a-zA-Z0-9_]+$ ]]; then
     echo "FATAL: PREFIX variable value [${bamboo_PREFIX}] is invalid. Only alpha numeric values and _ are allowed."
     exit 1
 fi
