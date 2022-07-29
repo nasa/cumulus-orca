9de170f2e524b0e8896d650a7d1ff5df50dd2830
AndrewDorn <dornadigital@gmail.com>
2022-07-29 11:49:21 -0500

    Feat: Added functional integration tests

--- integration_test/workflow_tests/helpers.py
+++ integration_test/workflow_tests/helpers.py
@@ -1,15 +1,88 @@
 import datetime
+import functools
+import random
 import time
+from typing import Callable, TypeVar
 
 import boto3
-import requests
+from requests import Session
+from requests.adapters import (
+    HTTPAdapter,
+    DEFAULT_POOLSIZE,
+    DEFAULT_POOLBLOCK,
+    DEFAULT_RETRIES,
+)
+
+MAX_RETRIES = 3  # number of times to retry.
+BACKOFF_FACTOR = 2  # Value of the factor used to backoff
+INITIAL_BACKOFF_IN_SECONDS = 1  # Number of seconds to sleep the first time through.
+RT = TypeVar("RT")  # return type
+
+
+# Retry decorator for function
+# todo: Lacks unit tests. Will likely eventually be part of shared lib ORCA-148.
+def retry_error(
+    max_retries: int = MAX_RETRIES,
+    backoff_in_seconds: int = INITIAL_BACKOFF_IN_SECONDS,
+    backoff_factor: int = BACKOFF_FACTOR,
+) -> Callable[[Callable[[], RT]], Callable[[], RT]]:
+    """
+    Decorator takes arguments to adjust number of retries and backoff strategy.
+    Args:
+        max_retries (int): number of times to retry in case of failure.
+        backoff_in_seconds (int): Number of seconds to sleep the first time through.
+        backoff_factor (int): Value of the factor used for backoff.
+    """
+
+    def decorator_retry_error(func: Callable[[], RT]) -> Callable[[], RT]:
+        """
+        Main Decorator that takes our function as an argument
+        """
+
+        @functools.wraps(func)  # Use built in for decorators
+        def wrapper_retry_error(*args, **kwargs) -> RT:
+            """
+            Wrapper that performs our extra tasks on the function
+            """
+            # Initialize the retry loop
+            total_retries = 0
+
+            # Enter loop
+            while True:
+                # Try the function and catch the expected error
+                # noinspection PyBroadException
+                try:
+                    # noinspection PyArgumentList
+                    return func(*args, **kwargs)
+                except Exception:
+                    if total_retries == max_retries:
+                        # Log it and re-raise if we maxed our retries + initial attempt
+                        print(
+                            f"Encountered Error {total_retries} times. Reached max retry limit."
+                        )
+                        raise
+                    else:
+                        # perform exponential delay
+                        backoff_time = (
+                            backoff_in_seconds * backoff_factor ** total_retries
+                            + random.uniform(0, 1)  # nosec
+                        )
+                        print(
+                            f"Encountered Error on attempt {total_retries}. Sleeping {backoff_time} seconds.",
+                        )
+                        time.sleep(backoff_time)
+                        total_retries += 1
+
+        # Return our wrapper
+        return wrapper_retry_error
+
+    # Return our decorator
+    return decorator_retry_error
 
 
 def get_state_machine_execution_results(
-    execution_arn, retry_interval_seconds=5, maximum_duration_seconds=30
+    execution_arn, retry_interval_seconds=5, maximum_duration_seconds=60
 ):
-    # todo: better backoff and such
-    # todo: decorator?
     start = datetime.datetime.utcnow()
     while True:
         # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/stepfunctions.html#SFN.Client.describe_execution
@@ -22,11 +95,55 @@ def get_state_machine_execution_results(
 
         if (
             datetime.datetime.utcnow() - start
-        ).total_seconds() > retry_interval_seconds:
+        ).total_seconds() > maximum_duration_seconds:
             raise TimeoutError()
 
         time.sleep(retry_interval_seconds)
 
 
-def post_to_api(api_invoke_url, data=...):  # todo: what?
-    return requests.post(api_invoke_url, data=data)
+@retry_error()
+def post_to_api(session: Session, api_invoke_url, data, headers=...):
+    result = session.post(api_invoke_url, data=data, headers=headers)
+    if result.status_code == 504:
+        raise Exception(f"API Gateway '{api_invoke_url}' timed out.")  # Raise to allow retry logic to catch
+    return result
+
+
+class DNSResolverHTTPSAdapter(HTTPAdapter):
+    """
+    client-side code to support application-specific DNS for the purpose of checking certificates
+    CommonName and SubjectAltName. The keyword argument you need in those calls is "assert_hostname".
+    Here's how to set it with the requests library
+    """
+
+    def __init__(
+        self,
+        common_name,
+        host,
+        pool_connections=DEFAULT_POOLSIZE,
+        pool_maxsize=DEFAULT_POOLSIZE,
+        max_retries=DEFAULT_RETRIES,
+        pool_block=DEFAULT_POOLBLOCK,
+    ):
+        self.__common_name = common_name
+        self.__host = host
+        super(DNSResolverHTTPSAdapter, self).__init__(
+            pool_connections=pool_connections,
+            pool_maxsize=pool_maxsize,
+            max_retries=max_retries,
+            pool_block=pool_block,
+        )
+
+    def get_connection(self, url, proxies=None):
+        redirected_url = url.replace(self.__common_name, self.__host)
+        return super(DNSResolverHTTPSAdapter, self).get_connection(
+            redirected_url, proxies=proxies
+        )
+
+    def init_poolmanager(
+        self, connections, maxsize, block=DEFAULT_POOLBLOCK, **pool_kwargs
+    ):
+        pool_kwargs["assert_hostname"] = self.__common_name
+        super(DNSResolverHTTPSAdapter, self).init_poolmanager(
+            connections, maxsize, block=block, **pool_kwargs
+        )
--- integration_test/workflow_tests/test_ingest_workflows.py
+++ integration_test/workflow_tests/test_ingest_workflows.py
@@ -5,37 +5,72 @@ import uuid
 from unittest import TestCase
 
 import boto3
+from requests import Session
 
 import helpers
 
 
 class TestIngestWorkflows(TestCase):
-    storage_bucket_name = None
-    aws_region = None
-    orca_api_deployment_invoke_url = None
-    orca_copy_to_glacier_step_function_arn = None
+    API_LOCAL_HOST = "127.0.0.1"
+    API_LOCAL_PORT = "8000"
+
+    storage_bucket_name: str = None
+    aws_region: str = None
+    orca_copy_to_glacier_step_function_arn: str = None
+
+    MY_SESSION: Session = None
+    AWS_API_NAME: str = None
+    API_URL: str = None
 
     def setUp(self) -> None:
-        os.environ["bamboo_STORAGE_BUCKET_NAME"] = "todo"
-        os.environ["bamboo_AWS_DEFAULT_REGION"] = "us-west-2"
-        os.environ["orca_API_DEPLOYMENT_INVOKE_URL"] = "https://s2jmh2r60k.execute-api.us-west-2.amazonaws.com/orca"  # todo: check format for this compared to tf output
-        os.environ[
-            "orca_COPY_TO_GLACIER_STEP_FUNCTION_ARN"
-        ] = "arn:aws:states:us-west-2:236859827343:stateMachine:doctest-OrcaCopyToGlacierWorkflow"
-
+        # Pull from environ
+        # ---
         TestIngestWorkflows.storage_bucket_name = os.environ[
             "bamboo_STORAGE_BUCKET_NAME"
         ]
         TestIngestWorkflows.aws_region = os.environ["bamboo_AWS_DEFAULT_REGION"]
-        TestIngestWorkflows.orca_api_deployment_invoke_url = os.environ[
-            "orca_API_DEPLOYMENT_INVOKE_URL"
-        ]
+        orca_api_deployment_invoke_url = os.environ["orca_API_DEPLOYMENT_INVOKE_URL"]
         TestIngestWorkflows.orca_copy_to_glacier_step_function_arn = os.environ[
             "orca_COPY_TO_GLACIER_STEP_FUNCTION_ARN"
         ]
 
+        # Set up Orca API resources
+        # ---
+        TestIngestWorkflows.AWS_API_NAME = orca_api_deployment_invoke_url.replace(
+            "https://", ""
+        )
+        TestIngestWorkflows.API_URL = f"https://{TestIngestWorkflows.AWS_API_NAME}:{TestIngestWorkflows.API_LOCAL_PORT}/orca"
+        TestIngestWorkflows.MY_SESSION = Session()
+        TestIngestWorkflows.MY_SESSION.mount(
+            TestIngestWorkflows.API_URL,
+            helpers.DNSResolverHTTPSAdapter(
+                TestIngestWorkflows.AWS_API_NAME, TestIngestWorkflows.API_LOCAL_HOST
+            ),
+        )
+
+    def test_catalog_provider_does_not_exist_returns_empty_granules_list(self):
+        """
+        Presently, we do not raise an error if the providerId is not present in our catalog.
+        """
+        catalog_output = helpers.post_to_api(
+            TestIngestWorkflows.MY_SESSION,
+            TestIngestWorkflows.API_URL + "/catalog/reconcile/",
+            data=json.dumps(
+                {
+                    "pageIndex": 0,
+                    "providerId": [uuid.uuid4().__str__()],
+                    "endTimestamp": int((time.time() + 5) * 1000),
+                }
+            ),
+            headers={"Host": TestIngestWorkflows.AWS_API_NAME},
+        )
+        self.assertEqual(200, catalog_output.status_code)
+        self.assertEqual({"granules": [], "anotherPage": False}, catalog_output.json())
+
     def test_ingest_no_granules_passes(self):
-        # todo: remember to check all these properties
+        """
+        If no granules are provided, should not store anything in DB.
+        """
         provider_id = uuid.uuid4().__str__()
         provider_name = uuid.uuid4().__str__()
 
@@ -43,34 +78,6 @@ class TestIngestWorkflows(TestCase):
         collection_version = uuid.uuid4().__str__()
 
         execution_id = uuid.uuid4().__str__()
-
-        # todo: remove the fluff, and use copy_to_glacier_input as needed
-        expected_output = {
-            "payload": {"granules": [], "copied_to_glacier": []},
-            "meta": {
-                "collection": {
-                    "name": collection_name,
-                    "version": collection_version
-                },
-                "provider": {
-                    "id": provider_id,
-                    "name": provider_name
-                }
-            },
-            "cumulus_meta": {
-                "execution_name": execution_id
-            },
-            "task_config": {
-                "excludeFileTypes": "{$.meta.collection.meta.excludeFileTypes}",
-                "s3MultipartChunksizeMb": "{$.meta.collection.meta.s3MultipartChunksizeMb}",
-                "providerId": "{$.meta.provider.id}", "providerName": "{$.meta.provider.name}",
-                "executionId": "{$.cumulus_meta.execution_name}",
-                "collectionShortname": "{$.meta.collection.name}",
-                "collectionVersion": "{$.meta.collection.version}",
-                "orcaDefaultBucketOverride": "{$.meta.collection.meta.orcaDefaultBucketOverride}",
-                "orcaDefaultStorageClassOverride": "{$.meta.collection.meta.orcaDefaultStorageClassOverride}"
-            },
-            "exception": "None"}
 
         # note that this does not cover all available properties
         copy_to_glacier_input = {
@@ -82,9 +89,28 @@ class TestIngestWorkflows(TestCase):
                     "name": provider_name,
                 },
             },
-            "cumulus_meta": {
-                "execution_name": execution_id
-            }
+            "cumulus_meta": {"execution_name": execution_id},
+        }
+
+        expected_output = {
+            "payload": {"granules": [], "copied_to_glacier": []},
+            "meta": {
+                "collection": {"name": collection_name, "version": collection_version},
+                "provider": {"id": provider_id, "name": provider_name},
+            },
+            "cumulus_meta": {"execution_name": execution_id},
+            "task_config": {
+                "excludeFileTypes": "{$.meta.collection.meta.excludeFileTypes}",
+                "s3MultipartChunksizeMb": "{$.meta.collection.meta.s3MultipartChunksizeMb}",
+                "providerId": "{$.meta.provider.id}",
+                "providerName": "{$.meta.provider.name}",
+                "executionId": "{$.cumulus_meta.execution_name}",
+                "collectionShortname": "{$.meta.collection.name}",
+                "collectionVersion": "{$.meta.collection.version}",
+                "orcaDefaultBucketOverride": "{$.meta.collection.meta.orcaDefaultBucketOverride}",
+                "orcaDefaultStorageClassOverride": "{$.meta.collection.meta.orcaDefaultStorageClassOverride}",
+            },
+            "exception": "None",
         }
 
         execution_info = boto3.client("stepfunctions").start_execution(
@@ -94,17 +120,26 @@ class TestIngestWorkflows(TestCase):
 
         print(f"Step function execution info: {execution_info}")
 
-        step_function_results = helpers.get_state_machine_execution_results(execution_info["executionArn"])
+        step_function_results = helpers.get_state_machine_execution_results(
+            execution_info["executionArn"]
+        )
         print(f"Step function results: {step_function_results}")
         print(f"Step function output: {step_function_results['output']}")
 
         self.assertEqual("SUCCEEDED", step_function_results["status"])
         self.assertEqual(expected_output, json.loads(step_function_results["output"]))
 
-        catalog_output = helpers.post_to_api(TestIngestWorkflows.orca_api_deployment_invoke_url + "/catalog/reconcile/", {
-            "pageIndex": 0,
-            "providerId": [provider_id],
-            "endTimestamp": int((time.time() + 5) * 1000)  # todo: untested
-        })
-
-        # todo: Make sure catalog_output is empty
+        catalog_output = helpers.post_to_api(
+            TestIngestWorkflows.MY_SESSION,
+            TestIngestWorkflows.API_URL + "/catalog/reconcile/",
+            data=json.dumps(
+                {
+                    "pageIndex": 0,
+                    "providerId": [provider_id],
+                    "endTimestamp": int((time.time() + 5) * 1000),
+                }
+            ),
+            headers={"Host": TestIngestWorkflows.AWS_API_NAME},
+        )
+        self.assertEqual(200, catalog_output.status_code)
+        self.assertEqual({"granules": [], "anotherPage": False}, catalog_output.json())
