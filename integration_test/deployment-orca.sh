ce3a1f1dfbc0521855818c15bbb887aa5f40b85a
AndrewDorn <dornadigital@gmail.com>
2022-08-15 16:45:51 -0500

    Feat: Theoretical implementation of integration test cleanup

--- integration_test/deployment-orca.sh
+++ integration_test/deployment-orca.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 set -ex
-export AWS_ACCESS_KEY_ID=$bamboo_CUMULUS_AWS_ACCESS_KEY_ID
-export AWS_SECRET_ACCESS_KEY=$bamboo_CUMULUS_AWS_SECRET_ACCESS_KEY
-export AWS_DEFAULT_REGION=$bamboo_CUMULUS_AWS_DEFAULT_REGION
-
-#remove old files from bamboo as they throw error
-rm *.tf
-
-git clone --branch ${bamboo_BRANCH_NAME} --single-branch https://github.com/nasa/cumulus-orca.git
-cd integration_test
-echo "Cloned Orca, branch ${bamboo_BRANCH_NAME}"
-
-#replace prefix with bamboo prefix variable
-sed -e 's/PREFIX/'"$bamboo_PREFIX"'/g' buckets.tf.template > buckets.tf
 
 if aws s3api head-bucket --bucket ${bamboo_PREFIX}-tf-state;then
     echo "terraform state bucket already present. Using existing state file"
@@ -32,15 +19,7 @@ else
       --region ${bamboo_AWS_DEFAULT_REGION}
 fi
 
-#configuring S3 backend
-echo "terraform {
-  backend \"s3\" {
-    bucket = \"${bamboo_PREFIX}-tf-state\"
-    region = \"${bamboo_AWS_DEFAULT_REGION}\"
-    key    = \"terraform.tfstate\"
-    dynamodb_table = \"${bamboo_PREFIX}-tf-locks\"
-  }
-}" >> terraform.tf
+cd integration_test
 
 terraform init -input=false
 echo "Deploying Cumulus S3 buckets and dynamoDB table"
@@ -48,26 +27,14 @@ terraform apply \
   -auto-approve \
   -input=false
 
-#clone cumulus orca template for deploying RDS cluster
-git clone --branch $bamboo_CUMULUS_ORCA_DEPLOY_TEMPLATE_VERSION --single-branch https://git.earthdata.nasa.gov/scm/orca/cumulus-orca-deploy-template.git
+#cumulus orca template for deploying RDS cluster
+cd ..
 cd cumulus-orca-deploy-template
-echo "cloned Cumulus, branch $bamboo_CUMULUS_ORCA_DEPLOY_TEMPLATE_VERSION"
 
 #deploy rds-cluster-tf module
 cd rds-cluster-tf
-echo "inside rds-cluster-tf"
-mv terraform.tfvars.example terraform.tfvars
 
-#replacing terraform.tf with proper values
-sed -e 's/PREFIX/'"$bamboo_PREFIX"'/g; s/us-east-1/'"$bamboo_AWS_DEFAULT_REGION"'/g' terraform.tf.example > terraform.tf
-
-export AWS_ACCOUNT_ID=$(aws sts get-caller-identity | jq -r '.Account')
-export VPC_ID=$(aws ec2 describe-vpcs | jq -r '.Vpcs | to_entries | .[] | .value.VpcId')
-export AWS_SUBNET_ID1=$(aws ec2 describe-subnets --filters "Name=availability-zone,Values=us-west-2a"| jq -r '.Subnets | .[] | select (.Tags | .[] | .Value | contains ("Private application ")) | .SubnetId ')
-export AWS_SUBNET_ID2=$(aws ec2 describe-subnets --filters "Name=availability-zone,Values=us-west-2b"| jq -r '.Subnets | .[] | select (.Tags | .[] | .Value | contains ("Private application ")) | .SubnetId ')
-export AWS_SUBNET_ID3=$(aws ec2 describe-subnets --filters "Name=availability-zone,Values=us-west-2c"| jq -r '.Subnets | .[] | select (.Tags | .[] | .Value | contains ("Private application ")) | .SubnetId ')
-
-# Initialize deployment
+# Initialize Terraform
 terraform init \
   -input=false
 
@@ -97,9 +64,6 @@ export RDS_SECURITY_GROUP=$(terraform output security_group_id)
 
 #deploy data persistence tf module
 cd ../data-persistence-tf
-mv terraform.tfvars.example terraform.tfvars
-#replacing terraform.tf with proper values
-sed -e 's/PREFIX/'"$bamboo_PREFIX"'/g; s/us-east-1/'"$bamboo_AWS_DEFAULT_REGION"'/g' terraform.tf.example > terraform.tf
 
 # Initialize deployment
 terraform init \
@@ -122,10 +86,6 @@ terraform apply \
 
 # script for deploying cumulus-tf module
 cd ../cumulus-tf
-
-#replacing .tf files with proper values
-sed 's/PREFIX/'"$bamboo_PREFIX"'/g' terraform.tfvars.example > terraform.tfvars
-sed -e 's/PREFIX/'"$bamboo_PREFIX"'/g; s/us-east-1/'"$bamboo_AWS_DEFAULT_REGION"'/g' terraform.tf.example > terraform.tf
 
 # Initialize deployment
 terraform init \
