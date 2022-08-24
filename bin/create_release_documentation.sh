e9e0c90fc6c011365e883172d387219bd80c65e6
Rizbi Hassan <rhassan@contractor.usgs.gov>
2022-08-24 11:23:08 -0500

    test

--- bin/create_release_documentation.sh
+++ bin/create_release_documentation.sh
@@ -73,9 +73,18 @@ export DEPLOYMENT_BRANCH=gh-pages
 export GIT_USER=$bamboo_SECRET_GITHUB_USER
 export GIT_PASS=$bamboo_SECRET_GITHUB_TOKEN
 
+echo "---------start-----------"
+echo $GIT_USER
+echo $GIT_PASS
+echo $bamboo_SECRET_GITHUB_EMAIL
+
+echo "---------end-----------"
+
 # We need to set some git config here so deploy doesn't complain when the commit occurs.
 git config --global user.email "$bamboo_SECRET_GITHUB_EMAIL"
 git config --global user.name "$GIT_USER"
+
+
 
 
 ## MAIN
