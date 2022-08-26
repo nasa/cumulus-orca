eae3b541ebc8c5a3bac47f386e84608bc811d5a7
Rizbi Hassan <rhassan@contractor.usgs.gov>
2022-08-26 08:31:13 -0500

    using su instead of sudo

--- bin/create_release_documentation.sh
+++ bin/create_release_documentation.sh
@@ -84,7 +84,8 @@ export GIT_PASS=$bamboo_GITHUB_TOKEN
 
 cd ~/
 ls -al
-su chown -R $(whoami) .config
+su 
+chown -R $(whoami) .config
 
 # We need to set some git config here so deploy doesn't complain when the commit occurs.
 git config --global user.email "$bamboo_GITHUB_EMAIL"
