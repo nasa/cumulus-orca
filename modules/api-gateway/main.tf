6660c0033fcc98a29e1cfc9970574a5d6c0348e9
dornadigital@gmail.com <dornadigital@gmail.com>
2022-11-10 15:02:26 -0600

    Update main.tf

--- modules/api-gateway/main.tf
+++ modules/api-gateway/main.tf
@@ -54,29 +54,6 @@ resource "aws_api_gateway_rest_api_policy" "orca_api_resource_policy" {
 EOF
 }
 
-# API details for graphql service
-resource "aws_api_gateway_resource" "api_graphql_resource" {
-  path_part   = "graphql"
-  parent_id   = aws_api_gateway_rest_api.orca_api.root_resource_id
-  rest_api_id = aws_api_gateway_rest_api.orca_api.id
-}
-
-resource "aws_api_gateway_method" "api_graphql_method" {
-  rest_api_id   = aws_api_gateway_rest_api.orca_api.id
-  resource_id   = aws_api_gateway_resource.api_graphql_resource.id
-  http_method   = "POST"
-  authorization = "AWS_IAM"
-}
-
-# resource "aws_api_gateway_integration" "orca_graphql_integration" {
-#   rest_api_id             = aws_api_gateway_rest_api.orca_api.id
-#   resource_id             = aws_api_gateway_resource.api_graphql_resource.id
-#   http_method             = aws_api_gateway_method.api_graphql_method.http_method
-#   integration_http_method = "POST"
-#   type                    = "AWS"
-#   uri                     = todo
-# }
-
 #API details for orca_catalog_reporting lambda
 resource "aws_api_gateway_resource" "orca_catalog_reporting_api_resource_catalog" {
   path_part   = "catalog"
