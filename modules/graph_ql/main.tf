078cd05730ecedb884ae0bb477b1585fb9053dd6
dornadigital@gmail.com <dornadigital@gmail.com>
2022-11-14 12:03:12 -0600

    Update main.tf

--- modules/graph_ql/main.tf
+++ modules/graph_ql/main.tf
@@ -241,7 +241,6 @@ resource "aws_security_group" "gql_task_security_group" {
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
-    # ipv6_cidr_blocks = ["::/0"]
   }
 
   tags = var.tags
