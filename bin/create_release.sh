338c37992a5e048160949ce8cfe47efa43c5735d
Rizbi Hassan <rizbiece@gmail.com>
2022-12-06 09:35:10 -0600

    added additional checks for release url

--- bin/create_release.sh
+++ bin/create_release.sh
@@ -44,8 +44,16 @@ if [[ ! $bamboo_RELEASE_FLAG == true ]]; then
   exit 0
 fi
 
+## Check if the code has already been released.
+export url="https://github.com/nasa/cumulus-orca/releases/tag/"v$bamboo_ORCA_VERSION""
+if curl --output /null --silent --fail "$url"; then
+  echo "Release URL already exists: $url. Exiting."
+  exit 0
+else
+  echo "$url does not exist. Proceeding with release..."
+fi
+
 ## Release the Code
-## TODO: Make this more robust. Check for errors see if release already created, etc.
 cd dist
 
 # Create Release
@@ -60,6 +68,14 @@ export RELEASE_URL=$(curl \
     | sed -e 's/.*\(https.*\)\"\,/\1/' \
     | sed -e 's/api/uploads/')
 
+# Release URL is created only if there is valid github token
+if [[ ! $RELEASE_URL ]]; then
+  echo "RELEASE_URL is empty. Exiting"
+  exit 1
+else
+  echo "$RELEASE_URL has not been released. Proceeding."
+fi
+
 # Release Package
 echo $RELEASE_URL
 curl \
@@ -71,3 +87,5 @@ curl \
 check_returncode $? "Error during curl command."
 
 cd ..
+
+exit 0
