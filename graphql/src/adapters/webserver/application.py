f7198c356c4d07dda20f6415f7d287ebac88e90e
dornadigital@gmail.com <dornadigital@gmail.com>
2022-12-15 10:34:05 -0600

    fix: Removed spaghetti dependency in main

--- graphql/src/adapters/webserver/application.py
+++ graphql/src/adapters/webserver/application.py
@@ -11,36 +11,36 @@ from src.adapters.storage.postgres import StorageAdapterPostgres
 from src.adapters.webserver.uvicorn_settings import UvicornSettings
 from src.adapters.word_generation.word_generation import UUIDWordGeneration
 
-initialized_logger_provider = BasicLoggerProvider()
-logger = initialized_logger_provider.get_logger("Setup")
 
-uvicorn_settings = UvicornSettings()  # todo: pass in via parameter
+def get_application(uvicorn_settings: UvicornSettings):
+    initialized_logger_provider = BasicLoggerProvider()
+    logger = initialized_logger_provider.get_logger("Setup")
 
-# todo: Make the loads and init a separate function in shared_db
-db_connect_info = json.loads(
-    uvicorn_settings.DB_CONNECT_INFO
-)
-db_connect_info = PostgresConnectionInfo(
-    admin_database_name=db_connect_info["admin_database"],
-    admin_username=db_connect_info["admin_username"],
-    admin_password=db_connect_info["admin_password"],
-    user_username=db_connect_info["user_username"],
-    user_password=db_connect_info["user_password"],
-    user_database_name=db_connect_info["user_database"],
-    host=db_connect_info["host"],
-    port=db_connect_info["port"],
-)
-validate_config(
-    db_connect_info,
-    logger
-)
+    # todo: Make the loads and init a separate function in shared_db
+    db_connect_info = json.loads(
+        uvicorn_settings.DB_CONNECT_INFO
+    )
+    db_connect_info = PostgresConnectionInfo(
+        admin_database_name=db_connect_info["admin_database"],
+        admin_username=db_connect_info["admin_username"],
+        admin_password=db_connect_info["admin_password"],
+        user_username=db_connect_info["user_username"],
+        user_password=db_connect_info["user_password"],
+        user_database_name=db_connect_info["user_database"],
+        host=db_connect_info["host"],
+        port=db_connect_info["port"],
+    )
+    validate_config(
+        db_connect_info,
+        logger
+    )
 
-user_connection_uri = create_postgres_connection_uri.create_user_uri(
-    db_connect_info, logger)
-adapters_storage = AdaptersStorage(
-    UUIDWordGeneration(),
-    StorageAdapterPostgres(user_connection_uri),
-    initialized_logger_provider
-)
+    user_connection_uri = create_postgres_connection_uri.create_user_uri(
+        db_connect_info, logger)
+    adapters_storage = AdaptersStorage(
+        UUIDWordGeneration(),
+        StorageAdapterPostgres(user_connection_uri),
+        initialized_logger_provider
+    )
 
-application = create_fastapi_app(uvicorn_settings, adapters_storage)
+    return create_fastapi_app(uvicorn_settings, adapters_storage)
--- graphql/src/adapters/webserver/main.py
+++ graphql/src/adapters/webserver/main.py
@@ -5,6 +5,7 @@ Description: Main entrypoint script for the application.
 
 import uvicorn
 
+from src.adapters.webserver.application import get_application
 from src.adapters.webserver.uvicorn_settings import UvicornSettings
 
 if __name__ == "__main__":
@@ -18,10 +19,10 @@ if __name__ == "__main__":
         #
         # A fresh module instance means the application cannot be set outside a static context.
         # Note that relative paths are not properly supported for this path.
-        app="application:application",  # todo: Remove this import string. The downsides far outweigh the benefits.
+        app=get_application(uvicorn_settings),
         host=uvicorn_settings.HOST,  # todo: Pass UvicornSettings into application via parameter
         port=uvicorn_settings.PORT,
         # Can only enable 'reload' functionality by passing as an import string.
         # Note: This doesn't catch changes to other modules.
-        reload=uvicorn_settings.DEV
+        reload=False
     )
