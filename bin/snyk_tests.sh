937f2e5a18263650382e63aa9d1c4ee3baf9da7f
Rizbi Hassan <rizbiece@gmail.com>
2023-01-06 10:18:15 -0600

    updated script to scan lambdas

--- bin/snyk_tests.sh
+++ bin/snyk_tests.sh
@@ -30,5 +30,22 @@ run_and_check_returncode "snyk auth $SNYK_AUTHENTICATION_TOKEN"
 echo "Scanning open-source code with Snyk for potential vulnerabilities..."
 cd website
 snyk test -d --severity-threshold=high || true
+cd -
 
-# TODO ORCA:598-Scan orca lambda requirements file using snyk
+function run_snyk_tests() {
+  echo
+  echo "Running tests in $1"
+  echo
+
+  cd $1
+  snyk test --severity-threshold=high || true
+  return_code=$?
+}
+export -f run_snyk_tests
+
+task_dirs=$(ls -d tasks/* | egrep -v "package")
+
+## Call each task's testing suite
+echo "Running snyk for lambdas"
+echo $pwd
+parallel --jobs 0 -n 1 -X --halt now,fail=1 run_snyk_tests ::: $task_dirs
