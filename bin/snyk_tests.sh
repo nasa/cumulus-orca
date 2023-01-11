9ebe91f67440451308c4258c3c4de3383203c02a
Rizbi Hassan <rizbiece@gmail.com>
2023-01-11 08:08:51 -0600

    updated snyk script to scan through all lambdas

--- bin/snyk_tests.sh
+++ bin/snyk_tests.sh
@@ -10,6 +10,8 @@
 ## brew tap snyk/tap
 ## brew install snyk
 ##
+## Run "pip install -r requirements-dev.txt" before running snyk test in order to install all packages first.
+##
 ## ENVIRONMENT SETTINGS
 ## -----------------------------------------------------------------------------
 ## SNYK_AUTHENTICATION_TOKEN (str) - snyk authentication token found from Snyk UI browser https://app.snyk.io/account.
@@ -22,30 +24,39 @@ source bin/common/check_returncode.sh
 # authenticate snyk CLI locally
 run_and_check_returncode "snyk auth $SNYK_AUTHENTICATION_TOKEN"
 
-# Iac run limited to 20 runs a month. TBD Discuss with Jon Velapondi.
-# echo "Scanning Infrastructure as code with Snyk for potential vulnerabilities..."
-# snyk iac test || true
-
 # scan orca website
 echo "Scanning open-source code with Snyk for potential vulnerabilities..."
 cd website
 snyk test -d --severity-threshold=high || true
 cd -
 
+# run snyk test for shared libraries
+echo
+echo "Running snyk tests in shared_libraries"
+echo
+cd shared_libraries
+snyk test --severity-threshold=high --file=requirements.txt --package-manager=pip || true
+cd -
+
 function run_snyk_tests() {
   echo
-  echo "Running tests in $1"
+  echo "Running snyk test in $1"
   echo
 
   cd $1
-  snyk test --severity-threshold=high || true
-  return_code=$?
+  # remove shared library from the requirement files since snyk cannot scan through it
+  grep -v '../../shared_libraries' requirements-dev.txt >> test-requirements-dev.txt
+  grep -v '../../shared_libraries' requirements.txt >> test-requirements.txt
+  snyk test --severity-threshold=high --file=test-requirements-dev.txt --package-manager=pip || true
+  snyk test --severity-threshold=high --file=test-requirements.txt --package-manager=pip || true
+  # remove test files
+  rm test-requirements*.txt
 }
 export -f run_snyk_tests
 
-task_dirs=$(ls -d tasks/* | egrep -v "package")
+task_dirs=$(ls -d tasks/*)
 
-## Call each task's testing suite
+## Call each task's snyk test suite
 echo "Running snyk for lambdas"
 echo $pwd
 parallel --jobs 0 -n 1 -X --halt now,fail=1 run_snyk_tests ::: $task_dirs
