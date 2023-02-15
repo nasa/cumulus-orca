3812986f9b0522c4fdcbde6b278438d0ddaf6b68
dornadigital@gmail.com <dornadigital@gmail.com>
2023-02-15 13:16:40 -0600

    Update extract_filepaths_for_granule.py

--- tasks/extract_filepaths_for_granule/extract_filepaths_for_granule.py
+++ tasks/extract_filepaths_for_granule/extract_filepaths_for_granule.py
@@ -246,7 +246,7 @@ def handler(event: Dict[str, Dict[str, Any]],
     Args:
         event: Event passed into the step from the aws workflow.
             See schemas/input.json and schemas/config.json for more information.
-            
+
         context: This object provides information about the lambda invocation, function,
             and execution env.
 
