4b973a4783b2315ee8e9a7cb042ab663da3bb2e7
dornadigital@gmail.com <dornadigital@gmail.com>
2023-03-03 11:24:15 -0600

    Update internal_reconcile_generation.py

--- graphql/src/use_cases/internal_reconcile_generation.py
+++ graphql/src/use_cases/internal_reconcile_generation.py
@@ -1,13 +1,12 @@
-import json
 import logging
-import os
 from datetime import datetime, timezone
+from typing import List
 
 from orca_shared.reconciliation import OrcaStatus
 
 from src.adapters.storage.internal_reconciliation_s3 import AWSS3FileLocation
+from src.entities.files import FileLocation
 from src.entities.internal_reconcile_report import InternalReconcileReportCursor
-from src.use_cases.adapter_interfaces.files import InternalReconciliationFilesInterface
 from src.use_cases.adapter_interfaces.storage import InternalReconcileGenerationStorageInterface
 
 MANIFEST_SOURCE_BUCKET_KEY = "sourceBucket"
@@ -15,66 +14,68 @@ MANIFEST_FILE_SCHEMA_KEY = "fileSchema"
 MANIFEST_CREATION_TIMESTAMP_KEY = "creationTimestamp"
 
 
-# todo: This process is heavily coupled to AWS and postgres.
-#  Decoupling will be required for clean architecture.
 class InternalReconcileGeneration:
     def __init__(
         self,
         storage: InternalReconcileGenerationStorageInterface,
-        files: InternalReconciliationFilesInterface,
     ):
         self.storage = storage
-        self.files = files
 
-    def get_current_archive_list(
+    def create_job(
         self,
-        manifest_location: AWSS3FileLocation,
-        report_bucket_region: str,
+        report_source: str,
+        creation_timestamp: int,
         logger: logging.Logger,
     ) -> InternalReconcileReportCursor:
         """
 
         Args:
-            manifest_location:
-            report_bucket_region:
-            logger:
+            report_source: The region covered by the report.
+            creation_timestamp:
+            logger: The logger to use.
 
         Returns:
 
         """
-        filename = os.path.basename(manifest_location.key)
-        if filename != "manifest.json":
-            logger.error(f"Illegal file '{filename}'. Must be 'manifest.json'")
-            raise Exception(f"Illegal file '{filename}'. Must be 'manifest.json'")
-
-        file_str = self.files.get_file_body(
-            manifest_location,
-            report_bucket_region,
-        )
-        manifest = json.loads(file_str)
-
         job_id = self.storage.create_job(
-            manifest[MANIFEST_SOURCE_BUCKET_KEY],
+            report_source,
             datetime.utcfromtimestamp(
-                int(manifest[MANIFEST_CREATION_TIMESTAMP_KEY]) / 1000
+                creation_timestamp
             ).replace(tzinfo=timezone.utc),
             logger,
         )
+        return InternalReconcileReportCursor(
+            job_id=job_id
+        )
 
+    def get_current_archive_list(
+        self,
+        report_cursor: InternalReconcileReportCursor,
+        columns_in_csv: List[str],
+        csv_file_locations: List[FileLocation],
+        report_bucket_region: str,
+        logger: logging.Logger,
+    ) -> None:
+        """
+
+        Args:
+            report_cursor: Cursor to the report to update.
+            columns_in_csv: Columns in the csv files.
+            csv_file_locations: Locations of the csv files in the report.
+            report_bucket_region: Required by current Postgres driver.
+            logger: The logger to use.
+        """
         try:
             self.storage.truncate_s3_partition(
-                manifest[MANIFEST_SOURCE_BUCKET_KEY],
+                report_cursor,
                 logger,
             )
 
-            csv_key_paths = self.files.setup_inventory_report_from_manifest(manifest, logger)
-
             self.storage.update_job_with_s3_inventory(
-                manifest_location.bucket_name,
+                report_cursor,
+                columns_in_csv,
+                csv_file_locations,
                 report_bucket_region,
-                csv_key_paths,
-                manifest[MANIFEST_FILE_SCHEMA_KEY],
-                job_id,
                 logger,
             )
         except Exception as fatal_exception:
@@ -82,12 +83,8 @@ class InternalReconcileGeneration:
             logger.error(f"Encountered a fatal error: {fatal_exception}")
             # noinspection PyArgumentList
             self.storage.update_job(
-                job_id,
+                report_cursor,
                 OrcaStatus.ERROR,
                 str(fatal_exception),
             )
             raise
-
-        return InternalReconcileReportCursor(
-            job_id=job_id, orca_archive_location=manifest[MANIFEST_SOURCE_BUCKET_KEY]
-        )
