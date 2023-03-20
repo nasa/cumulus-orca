afa14c546a0912ebcfc7ac3a2363f608a91e95cd
dornadigital@gmail.com <dornadigital@gmail.com>
2023-03-20 09:12:30 -0500

    test: Remaining IR GQL test updates

--- graphql/test/unit_tests/adapters/graphql/resolvers/test_internal_reconcile_report.py
+++ graphql/test/unit_tests/adapters/graphql/resolvers/test_internal_reconcile_report.py
@@ -2,6 +2,7 @@ import unittest
 from unittest.mock import MagicMock, Mock, patch
 
 from src.adapters.graphql.resolvers import internal_reconcile_report
+from src.entities.internal_reconcile_report import InternalReconcileReportCursor
 
 
 class TestInternalReconcileReport(unittest.TestCase):
@@ -56,8 +57,10 @@ class TestInternalReconcileReport(unittest.TestCase):
         self.assertEqual(mock_error.return_value, result)
 
     @patch("src.adapters.graphql.resolvers.internal_reconcile_report.InternalReconcileGeneration")
+    @patch("src.adapters.graphql.resolvers.internal_reconcile_report.EdgeCursor.decode_cursor")
     def test_update_job_happy_path(
         self,
+        mock_decode_cursor: MagicMock,
         mock_use_case: MagicMock,
     ):
         """
@@ -71,18 +74,23 @@ class TestInternalReconcileReport(unittest.TestCase):
         result = \
             internal_reconcile_report.update_job(mock_report_cursor, mock_status,
                                                  mock_error_message,
-                                                 mock_storage_irr)
+                                                 mock_storage_irr,
+                                                 Mock())
 
+        mock_decode_cursor.assert_called_once_with(mock_report_cursor,
+                                                   InternalReconcileReportCursor)
         mock_use_case.assert_called_once_with(mock_storage_irr)
         mock_use_case.return_value.update_job.assert_called_once_with(
-            mock_report_cursor, mock_status, mock_error_message)
+            mock_decode_cursor.return_value, mock_status, mock_error_message)
         self.assertEqual(None, result)
 
     @patch("src.adapters.graphql.resolvers.internal_reconcile_report"
            ".InternalServerErrorGraphqlType")
     @patch("src.adapters.graphql.resolvers.internal_reconcile_report.InternalReconcileGeneration")
+    @patch("src.adapters.graphql.resolvers.internal_reconcile_report.EdgeCursor.decode_cursor")
     def test_update_job_error_wrapped(
         self,
+        mock_decode_cursor: MagicMock,
         mock_use_case: MagicMock,
         mock_error: MagicMock,
     ):
@@ -94,22 +102,30 @@ class TestInternalReconcileReport(unittest.TestCase):
         mock_error_message = Mock()
         mock_storage_irr = Mock()
         mock_exception = Exception()
+        mock_logger = Mock()
+        mock_logger.error = Mock()
 
         mock_use_case.side_effect = mock_exception
 
         result = \
             internal_reconcile_report.update_job(mock_report_cursor, mock_status,
                                                  mock_error_message,
-                                                 mock_storage_irr)
+                                                 mock_storage_irr,
+                                                 mock_logger)
 
+        mock_decode_cursor.assert_called_once_with(mock_report_cursor,
+                                                   InternalReconcileReportCursor)
         mock_use_case.assert_called_once_with(mock_storage_irr)
         mock_use_case.return_value.update_job.assert_not_called()
         mock_error.assert_called_once_with(mock_exception)
+        mock_logger.error.assert_called_once_with(mock_exception)
         self.assertEqual(mock_error.return_value, result)
 
     @patch("src.adapters.graphql.resolvers.internal_reconcile_report.InternalReconcileGeneration")
+    @patch("src.adapters.graphql.resolvers.internal_reconcile_report.EdgeCursor.decode_cursor")
     def test_get_current_archive_list_happy_path(
         self,
+        mock_decode_cursor: MagicMock,
         mock_use_case: MagicMock,
     ):
         """
@@ -134,22 +150,26 @@ class TestInternalReconcileReport(unittest.TestCase):
                 mock_logger,
             )
 
+        mock_decode_cursor.assert_called_once_with(mock_report_cursor,
+                                                   InternalReconcileReportCursor)
         mock_use_case.assert_called_once_with(mock_storage_irr)
         mock_use_case.return_value.get_current_archive_list.assert_called_once_with(
             mock_report_source,
-            mock_report_cursor,
+            mock_decode_cursor.return_value,
             mock_columns_in_csv,
             mock_csv_file_locations,
             mock_report_bucket_region,
             mock_logger,
-            )
+        )
         self.assertEqual(None, result)
 
     @patch("src.adapters.graphql.resolvers.internal_reconcile_report"
            ".InternalServerErrorGraphqlType")
     @patch("src.adapters.graphql.resolvers.internal_reconcile_report.InternalReconcileGeneration")
+    @patch("src.adapters.graphql.resolvers.internal_reconcile_report.EdgeCursor.decode_cursor")
     def test_get_current_archive_list_error_wrapped(
         self,
+        mock_decode_cursor: MagicMock,
         mock_use_case: MagicMock,
         mock_error: MagicMock,
     ):
@@ -178,14 +198,18 @@ class TestInternalReconcileReport(unittest.TestCase):
                 mock_logger,
             )
 
+        mock_decode_cursor.assert_called_once_with(mock_report_cursor,
+                                                   InternalReconcileReportCursor)
         mock_use_case.assert_called_once_with(mock_storage_irr)
         mock_use_case.return_value.update_job.assert_not_called()
         mock_error.assert_called_once_with(mock_exception)
         self.assertEqual(mock_error.return_value, result)
 
     @patch("src.adapters.graphql.resolvers.internal_reconcile_report.InternalReconcileGeneration")
+    @patch("src.adapters.graphql.resolvers.internal_reconcile_report.EdgeCursor.decode_cursor")
     def test_perform_orca_reconcile_happy_path(
         self,
+        mock_decode_cursor: MagicMock,
         mock_use_case: MagicMock,
     ):
         """
@@ -204,12 +228,14 @@ class TestInternalReconcileReport(unittest.TestCase):
                 mock_logger,
             )
 
+        mock_decode_cursor.assert_called_once_with(mock_report_cursor,
+                                                   InternalReconcileReportCursor)
         mock_use_case.assert_called_once_with(mock_storage_irr)
         mock_use_case.return_value.perform_orca_reconcile.assert_called_once_with(
             mock_report_source,
-            mock_report_cursor,
+            mock_decode_cursor.return_value,
             mock_logger,
-            )
+        )
         self.assertEqual(None, result)
 
     @patch("src.adapters.graphql.resolvers.internal_reconcile_report"
