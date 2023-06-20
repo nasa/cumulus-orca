2bf37c427f0edf1c80d44f3f49917d96c6cbbe49
Rizbi Hassan <rizbiece@gmail.com>
2023-06-20 09:24:52 -0500

    test2

--- integration_test/shared/setup-orca.sh
+++ integration_test/shared/setup-orca.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 echo "Hello"
+export AWS_ACCESS_KEY_ID=$bamboo_CUMULUS_AWS_ACCESS_KEY_ID
+export AWS_SECRET_ACCESS_KEY=$bamboo_CUMULUS_AWS_SECRET_ACCESS_KEY
 export AWS_DEFAULT_REGION=$bamboo_CUMULUS_AWS_DEFAULT_REGION
 
 export AWS_ACCOUNT_ID=$(aws sts get-caller-identity | jq -r '.Account')
