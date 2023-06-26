79f21128557697c0d1ef57791e96e9054c29f803
dornadigital@gmail.com <dornadigital@gmail.com>
2023-06-26 12:37:32 -0500

    feat: Added aws_profile to ORCA AWS provider

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 provider "aws" {
-  region = var.aws_region
+  region  = var.aws_region
+  profile = var.aws_profile
 
   ignore_tags {
     key_prefixes = ["gsfc-ngap"]
--- variables.tf
+++ variables.tf
@@ -44,6 +44,12 @@ variable "vpc_id" {
 
 
 ## OPTIONAL
+variable "aws_profile" {
+  type    = string
+  default = null
+}
+
+
 variable "tags" {
   type        = map(string)
   description = "Tags to be applied to resources that support tags."
