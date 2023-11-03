9abcbcebb0097290d6b929bc61b86123476a5ef3
Rizbi Hassan <rizbiece@gmail.com>
2023-11-03 07:59:28 -0500

    test region

--- integration_test/deploy-dr-buckets.sh
+++ integration_test/deploy-dr-buckets.sh
@@ -7,7 +7,7 @@ fi
 
 export AWS_ACCESS_KEY_ID=$bamboo_DR_AWS_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY=$bamboo_DR_AWS_SECRET_ACCESS_KEY
-export AWS_DEFAULT_REGION=$bamboo_AWS_DEFAULT_REGION
+# export AWS_DEFAULT_REGION=$bamboo_AWS_DEFAULT_REGION
 
 #remove old files from bamboo as they throw error
 rm *.tf
@@ -17,7 +17,7 @@ sed -e 's/PREFIX/'"$bamboo_PREFIX"'/g' dr-buckets.tf.template > dr-buckets.tf
 
 if ! aws s3api head-bucket --bucket ${bamboo_PREFIX}-dr-tf-state;then
     echo "terraform state bucket is not created. Creating ..."
-    aws s3api create-bucket --bucket ${bamboo_PREFIX}-dr-tf-state  --region ${bamboo_AWS_DEFAULT_REGION} --create-bucket-configuration LocationConstraint=${bamboo_AWS_DEFAULT_REGION}
+    aws s3api create-bucket --bucket ${bamboo_PREFIX}-dr-tf-state --create-bucket-configuration LocationConstraint=${bamboo_AWS_DEFAULT_REGION}
     
     aws s3api put-bucket-versioning \
     --bucket ${bamboo_PREFIX}-dr-tf-state \
@@ -27,8 +27,8 @@ if ! aws s3api head-bucket --bucket ${bamboo_PREFIX}-dr-tf-state;then
       --table-name ${bamboo_PREFIX}-dr-tf-locks \
       --attribute-definitions AttributeName=LockID,AttributeType=S \
       --key-schema AttributeName=LockID,KeyType=HASH \
-      --billing-mode PAY_PER_REQUEST \
-      --region ${bamboo_AWS_DEFAULT_REGION}
+      --billing-mode PAY_PER_REQUEST
+      # --region ${bamboo_AWS_DEFAULT_REGION}
 else
     echo "terraform state bucket present."
 fi
@@ -37,7 +37,6 @@ fi
 echo "terraform {
   backend \"s3\" {
     bucket = \"${bamboo_PREFIX}-dr-tf-state\"
-    region = \"${bamboo_AWS_DEFAULT_REGION}\"
     key    = \"terraform.tfstate\"
     dynamodb_table = \"${bamboo_PREFIX}-dr-tf-locks\"
   }
