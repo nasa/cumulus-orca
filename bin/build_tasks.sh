06713722ff4499f397ea6f8903e045c27296843d
manoonabbasi <mansoorabbasi94@gmail.com>
2023-11-06 20:42:56 +0500

    Fixed overengineering, and made the code more clear and efficient. Real parallelized...

--- bin/build_tasks.sh
+++ bin/build_tasks.sh
@@ -1,36 +1,31 @@
 #!/bin/sh
-#TODO This needs to be better organized
 set -e
 
 source bin/common/check_returncode.sh
 
-echo "pwd `pwd`"
+echo "pwd $(pwd)"
 
 echo "Pulling awslambda-psycopg2 in preparation for build"
 mkdir -p tasks/package
-cd tasks/package
-if [ ! -d "../package/awslambda-psycopg2/psycopg2-3.9" ]; then
-  rm -d -f -r "../package/awslambda-psycopg2"
+if [ -d "tasks/package/awslambda-psycopg2" ]; then
+  rm -rf "tasks/package/awslambda-psycopg2"
 fi
-if [ ! -d "awslambda-psycopg2" ]
-then
-  git clone https://github.com/jkehler/awslambda-psycopg2.git
+if [ ! -d "tasks/package/awslambda-psycopg2" ]; then
+  git clone https://github.com/jkehler/awslambda-psycopg2.git "tasks/package/awslambda-psycopg2"
 fi
-cd ../../
 
 function build_task() {
+  local task_dir="$1"
   echo
-  echo "Building $1"
+  echo "Building $task_dir"
   echo
   
-  cd $1
-  bin/build.sh
+  (cd "$task_dir" && bin/build.sh)
   return_code=$?
-  if [ $return_code != 0 ]
-  then
-    echo "ERROR: Building of $1 failed with code $return_code"
+  if [ $return_code -ne 0 ]; then
+    echo "ERROR: Building of $task_dir failed with code $return_code"
   fi
-  echo "Completed building $1"
+  echo "Completed building $task_dir"
   return $return_code
 }
 export -f build_task
@@ -40,12 +35,9 @@ echo "Build targets: $task_dirs"
 
 echo "Building in parallel..."
 # todo: ORCA-681: Repair parallelism and switch back to `--jobs 0`
-parallel --jobs 1 -n 1 -X --halt now,fail=1 build_task ::: $task_dirs
-
 process_return_code=$?
 echo
-if [ $process_return_code -ne 0 ]
-then
+if [ $process_return_code -ne 0 ]; then
   echo "ERROR: process failed with code $process_return_code."
   exit 1  # process_return_code may indicate how many tasks failed. Flatten to 1 if any number failed.
 else
