e7b472e526906b71a617efddde82af9b278f5df7
Zachary Roehrich <zroehrich@contractor.usgs.gov>
2023-12-11 10:43:47 -0600

    Reverting accidental commit to master

--- integration_test/workflow_tests/custom_logger.py
+++ integration_test/workflow_tests/custom_logger.py
@@ -11,6 +11,7 @@ class CustomLoggerAdapter(logging.LoggerAdapter):
     @staticmethod
     def set_logger(group_name):
         logger = logging.getLogger(__name__)
+        #logger.propagate = False
         logger_adapter = CustomLoggerAdapter(logger, {"my_context": group_name})
         if not logger.handlers:
             syslog = logging.StreamHandler()
