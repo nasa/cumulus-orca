f60a4c42ad6222caf304ca8104a20c9ea8f2e9df
Zachary Roehrich <131377950+zroehrich@users.noreply.github.com>
2024-02-02 10:54:12 -0600

    removed profile input

--- bin/delete_lambda.py
+++ bin/delete_lambda.py
@@ -4,18 +4,15 @@ import json
 # Gets user input for the prefix of ORCA lambdas to delete
 prefix = input("Enter Prefix: ")
 
-# Gets AWS Profile to use for ORCA lambda deletion
-profile = input("Enter AWS Profile: ")
-
 # Gets ORCA lambda functions with given prefix
-get_functions = f"aws lambda list-functions --profile {profile} --query 'Functions[] | [?contains(FunctionName, `{prefix}`) == `true`]'"
+get_functions = f"aws lambda list-functions --query 'Functions[] | [?contains(FunctionName, `{prefix}`) == `true`]'"
 completed_process = subprocess.run(get_functions, shell=True, capture_output=True)
 output = completed_process.stdout
 convert = json.loads(output.decode("utf-8").replace("'","'"))
 
 for sub in convert:
     lambda_output = sub['FunctionArn']
-    get_tags = f"aws lambda list-tags --profile {profile} --resource {lambda_output}"
+    get_tags = f"aws lambda list-tags --resource {lambda_output}"
     tags_process = subprocess.run(get_tags, shell=True, capture_output=True)
     tag_output = tags_process.stdout
     tag_convert = json.loads(tag_output.decode("utf-8").replace("'","'"))
@@ -25,7 +22,7 @@ for sub in convert:
             if tag_convert[tag]['application'] == 'ORCA':
                 print("Deleting " + sub['FunctionName'])
                 del_lambda = sub['FunctionName']
-                delete_function = f"aws lambda delete-function --profile {profile} --function-name {del_lambda}"
+                delete_function = f"aws lambda delete-function --function-name {del_lambda}"
                 subprocess.run(delete_function, shell=True, capture_output=True)
     # If the lambda with the provided prefix does not match the tag for deletion it is skipped and is not deleted
     except KeyError:
