50705b9c8f301d4c8e8a3506aa96559d247836f0
Rizbi Hassan <rhassan@contractor.usgs.gov>
2024-07-22 10:06:09 -0500

    terraform for creating vpc link

---
+++ modules/vpc_link/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.63.0, < 4.0.0"
+    }
+  }
+}
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-west-2"
+}
+
+#create VPC link in DR account
+resource "awscc_apigatewayv2_vpc_link" "orca-vpc-link" {
+  name               = "rhassan-vpc-link-demo"
+  subnet_ids         = ["subnet-03141fdc343bb715d"]
+  # security_group_ids = [var.securit_group_id]
+  tags = {
+    key   = "Team"
+    value = "ORCA"
+  }
+}
+
+# output "repo_url" {
+#     value = aws_ecr_repository.prototype_repo.repository_url
+# }
