b95ca9b3c4421fdd38c3a5bb988980dec76aa359
Zachary Roehrich <131377950+zroehrich@users.noreply.github.com>
2024-09-30 11:55:13 -0500

    Create variables.tf

---
+++ modules/scheduler/module/variables.tf
@@ -0,0 +1,29 @@
+variable "aws_account_id" {
+    type = string
+}
+
+variable "create_office_hours" {
+    default = false
+}
+
+variable "create_office_hours_8_6" {
+    default = false
+}
+
+variable "create_office_hours_autoscaling" {
+    default = false
+}
+
+variable "create_office_hours_8_6_autoscaling" {
+    default = false
+}
+
+variable "role_permissions_boundary_arn" {
+  description = "CSR developer permissions boundary"
+  type = string
+}
+
+variable "tags" {
+  description = "Default tags to be applied."
+  type = map(string)
+}
