3604c885e910690dfb1457a7e8843ac9f24c297e
Zachary Roehrich <131377950+zroehrich@users.noreply.github.com>
2025-01-09 13:55:47 -0600

    Update main.tf

--- modules/orca/main.tf
+++ modules/orca/main.tf
@@ -36,6 +36,7 @@ module "orca_lambdas" {
   orca_sqs_status_update_queue_id     = module.orca_sqs.orca_sqs_status_update_queue_id
   orca_sqs_status_update_queue_arn    = module.orca_sqs.orca_sqs_status_update_queue_arn
   restore_object_role_arn             = module.orca_iam.restore_object_role_arn
+  lambda_log_retention_in_days        = var.lambda_log_retention_in_days
 
   ## OPTIONAL
   lambda_runtime                                = var.lambda_runtime
