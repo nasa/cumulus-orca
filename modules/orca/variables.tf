64ec05eb05f011d59d4d84dfcaa5fa4392340187
Zachary Roehrich <131377950+zroehrich@users.noreply.github.com>
2025-05-01 15:53:00 -0500

    Update variables.tf

--- modules/orca/variables.tf
+++ modules/orca/variables.tf
@@ -307,5 +307,5 @@ variable "deploy_rds_dedicated_instance_role_association" {
 
 variable "lambda_xray" {
   type        = string
-  description = "Enables or disables X-Ray on ORCA Lambdas, value set to Active for enabled and Passthrough for disabled."
+  description = "Enables or disables X-Ray on ORCA Lambdas, value set to Active for enabled and PassThrough for disabled."
 }
