5c13e5b7e9b0dc180b2f79c8d2444fb0dd92f0b1
Rizbi Hassan <rhassan@contractor.usgs.gov>
2025-09-29 08:31:36 -0500

    fixed api gateway URL

--- modules/api-gateway/output.tf
+++ modules/api-gateway/output.tf
@@ -1,4 +1,4 @@
 output "orca_api_deployment_invoke_url" {
-  value       = aws_api_gateway_deployment.orca_api_deployment.invoke_url
+  value       = aws_api_gateway_stage.orca_api_stage_name.invoke_url
   description = "The URL to invoke the ORCA Cumulus reconciliation API gateway. Excludes the resource path"
 }
